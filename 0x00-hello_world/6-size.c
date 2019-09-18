9b1b60849e83a6d4bd4ffb887900c26773f82822
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2019-09-18 20:10:43 +0000

    Correction to 6-Size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,10 +7,10 @@
  */
 int main(void)
 {
-	printf("Size of a char: %ld byte(s)", (SIZE(char)));
-	printf("Size of a int: %ld byte(s)", (SIZE(int)));
-	printf("Size of a long: %ld byte(s)", (SIZE(long)));
-	printf("Size of a long long: %ld byte(s)", (SIZE(long long)));
-	printf("Size of a float: %ld byte(s)", (SIZE(float)));
+	printf("Size of a char: %ld byte(s)", sizeof(char));
+	printf("Size of a int: %ld byte(s)", sizeof(int));
+	printf("Size of a long: %ld byte(s)", sizeof(long int));
+	printf("Size of a long long: %ld byte(s)", sizeof(long long int));
+	printf("Size of a float: %ld byte(s)", sizeof(float));
 	return (0);
 }
