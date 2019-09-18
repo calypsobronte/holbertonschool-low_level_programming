2d28322809e6076c9821dcb5e00cdb679f667a96
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2019-09-18 20:03:59 +0000

    5-Hello, printf

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,1 +1,16 @@
-#!/bin/bash
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("Size of a char: %ld byte(s)", (SIZE(char)));
+	printf("Size of a int: %ld byte(s)", (SIZE(int)));
+	printf("Size of a long: %ld byte(s)", (SIZE(long)));
+	printf("Size of a long long: %ld byte(s)", (SIZE(long long)));
+	printf("Size of a float: %ld byte(s)", (SIZE(float)));
+	return (0);
+}
