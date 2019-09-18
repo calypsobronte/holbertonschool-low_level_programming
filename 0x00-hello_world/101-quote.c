e93eeac57e539ff90968dbee65e1f3ee9957871a
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2019-09-18 22:03:00 +0000

    update  function which allowed me to print in the standart error write

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,10 @@
-#include <stdio.h>
-
+#include <unistd.h>
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Return: Always 1 (Success)
  */
-int main(void)
-{
-	return (0);
+int main(void) {
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 58);
+	return 1;
 }
-
