d360c530f6d3f1a277ed3794af8a7542db266330
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2019-09-18 19:13:58 +0000

    main problem comments

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
+
 /**
- * Main / File
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
-int main() {
-	  puts("\"Programming is like building a multilingual puzzle");
-	  return 0;
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
