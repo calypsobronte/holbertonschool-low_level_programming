8d474b86efb4bec7126a11ccce17a3104d481e03
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2019-09-18 18:36:51 +0000

    4-puts.c with c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,1 +1,10 @@
-#!/bin/bash
+#include <stdio.h>
+
+/**
+ * Student: Lina M. Montaño Ramirez - Calypso Bronte
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle")
+	return (0);
+}
