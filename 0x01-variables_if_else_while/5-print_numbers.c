8ebaeda4ce506dc7daf825908a3f2787fb340bdb
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2019-09-19 19:40:14 +0000

    5. Numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,1 +1,14 @@
-#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    char c;
+    for (c = 0; c < 9; c++)
+    putchar("%i",c);
+    putchar('\n');
+    return (0);
+}
