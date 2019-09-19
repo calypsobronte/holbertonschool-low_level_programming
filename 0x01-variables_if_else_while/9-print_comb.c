375960f1276184ff969baa5fe7e3719cf0ddbc7f
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2019-09-19 21:06:35 +0000

    bugs .. Patience, persistence and perspiration make an unbeatable combination for success

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char c = 0;
+
+while (c < 10)
+putchar(c);
+if (c < 9)
+putchar(',');
+putchar(' ');
+c++;
+putchar('\n');
+return (0);
+}
