620861f0d3c3fd4043d108d6f6a5a2281e89fc70
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2019-09-19 20:16:50 +0000

    5. Numbersz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,1 +1,15 @@
-#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int c = '0';
+while (c <= '9')
+putchar(c);
+c++;
+putchar('\n');
+return (0);
+}
