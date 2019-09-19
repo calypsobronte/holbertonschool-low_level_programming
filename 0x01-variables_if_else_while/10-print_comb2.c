2afe8faa7cd8092cb7e6ac7a074adaeca9d3ba55
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2019-09-19 21:59:59 +0000

    10. 00...99

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -1,1 +1,26 @@
+#include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i, c, j;
+for (i = 0; i < 100; i++)
+{
+c = i / 10;
+j = i % 10;
+putchar(c + '0');
+putchar(j + '0');
+if (i != 99)
+{
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
