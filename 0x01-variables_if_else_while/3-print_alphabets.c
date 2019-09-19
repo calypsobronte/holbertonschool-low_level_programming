ae94d8ea46517c7fa7b68d1a6f00370bebdf4377
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2019-09-19 19:17:11 +0000

    correction in syntax different 3-print-

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,13 +8,12 @@
  */
 int main(void)
 {
-  char c;
-  char d;
-
-  for (c = 'a'; c <= 'z'; c++)
-    putchar(c);
-  for (d = 'A'; d <='Z';d++)
-    putchar(d);
-  putchar('\n');
-  return (0);
+char c;
+char d;
+for (c = 'a'; c <= 'z'; c++)
+putchar(c);
+for (d = 'A'; d <= 'Z'; d++)
+putchar(d);
+putchar('\n');
+return (0);
 }
