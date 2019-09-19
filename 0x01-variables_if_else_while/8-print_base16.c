c397caeb0889852588a44ac4fb8b23b1f5043be2
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2019-09-19 20:43:19 +0000

    8. Hexadecimal with ASCII

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char c;
+char d;
+for (c = 48; c < 58; c++)
+putchar(c);
+for (d = 65; d < 71; d++)
+putchar(d);
+putchar('\n');
+return (0);
+}
