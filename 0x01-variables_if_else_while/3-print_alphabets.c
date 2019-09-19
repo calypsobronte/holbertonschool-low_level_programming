337d390a147763ef4d640d78ee6f8d30cb51b51e
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2019-09-19 16:27:48 +0000

    3. alphABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+  char c;
+  char d;
+
+  for (c = 'a'; c <= 'z'; c++)
+    putchar(c);
+  for (d = 'A'; d <='Z';d++)
+    putchar(d);
+  putchar('\n');
+  return (0);
+}
