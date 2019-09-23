f3e28daea783321fe71d1b98e23786561e7c7d75
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2019-09-23 23:17:11 +0000

    6. There is no such thing as absolute value in this world. You can only estimate what a thing is worth to you

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,1 +1,22 @@
-#calypsobronte
+#include "holberton.h"
+/**
+ * main - empty file
+ *
+ * Return: ALways 0 (Success)
+ */
+int print_last_digit(int n)
+{
+    int return1;
+    
+    if (n > 0)
+    {
+        return1 = n % 10;
+        _putchar(return1 + '0');
+    }
+    else
+    {
+        return1 = (n * -1) % 10;
+        _putchar(return1 + '0');
+    }
+    return return1;
+}
