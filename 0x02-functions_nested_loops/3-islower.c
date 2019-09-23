9080a284d56a760ddb345d04addf5ce38c5afc0e
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2019-09-23 21:19:45 +0000

    3. islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,1 +1,21 @@
-#calypsobronte
+#include "holberton.h"
+/**
+ * main - empty file
+ *
+ * Return: ALways 0 (Success)
+ */
+int _islower(int c)
+{
+
+if (c > 'a' && c < 'z')
+{
+return (1);
+_putchar('\n');
+}
+else
+{
+return (0);
+_putchar('\n');
+}
+}
+
