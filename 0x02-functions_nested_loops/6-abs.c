c251d4398c549cf2230a9f62129cead146be5caf
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2019-09-23 23:01:07 +0000

    6. There is no such thing as absolute value in this world. You can only estimate what a thing is worth to you

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,1 +1,17 @@
-#calypsobronte
+#include "holberton.h"
+/**
+ * main - empty file
+ *
+ * Return: ALways 0 (Success)
+ */
+int _abs(int n)
+{
+    if (n >= 0)
+    {
+        return (n);
+    }
+    else
+    {
+        return (n * -1);
+    }
+}
