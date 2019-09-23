3d1b777cf59eb60858560958998dac32f5371d12
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2019-09-23 19:44:06 +0000

    0. _putchar

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,1 +1,20 @@
-#calypsobronte
+#include "holberton.h"
+/**
+ * main - empty file
+ *
+ * Return: ALways 0 (Success)
+ */
+int main(void)
+{
+_putchar('H');
+_putchar('o');
+_putchar('l');
+_putchar('b');
+_putchar('e');
+_putchar('r');
+_putchar('t');
+_putchar('o');
+_putchar('n');
+_putchar('\n');
+return (0);
+}
