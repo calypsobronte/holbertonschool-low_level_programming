7c4f40dde2efa4f24a4b2994b8f706d70c8880f7
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2019-09-23 21:36:55 +0000

    4. isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,1 +1,20 @@
-#calypsobronte
+#include "holberton.h"
+/**
+ * main - empty file
+ *
+ * Return: ALways 0 (Success)
+ */
+int _isalpha(int c)
+{
+
+if ((c >= 'a' && c <= 'z') || (c > 'A' && c < 'Z'))
+{
+return (1);
+_putchar('\n');
+}
+else
+{
+return (0);
+_putchar('\n');
+}
+}
