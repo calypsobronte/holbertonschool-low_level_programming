08c9ba9965a5e5dd8df83c9324ccd771b92c225c
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2019-09-24 23:33:32 +0000

    add 1-main

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -4,7 +4,6 @@
 * main - causes an infinite loop
 * Return: 0
 */
-
 int main(void)
 {
 int i;
@@ -12,14 +11,12 @@ int i;
 printf("Infinite loop incoming :(\n");
 
 i = 0;
-
 /*
 * while (i < 10)
 {
 putchar(i);
 }
 */
-
 printf("Infinite loop avoided! \\o/\n");
 
 return (0);
