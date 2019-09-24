94a0ad58d533180f83478a42e0049c6b4881dde4
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2019-09-24 00:01:13 +0000

    8. I'm federal agent Jack Bauer, and today is the longest day of my life

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,1 +1,30 @@
-#calypsobronte
+#include "holberton.h"
+/**
+* _putchar - writes the character c to stdout
+* @c: The character to print
+*
+* Return: On success 1.
+* On error, -1 is returned, and errno is set appropriately.
+*/
+void jack_bauer(void)
+{
+    int i, k, o, p, j;
+    int n;
+    for (n = 0; n <= 23 ; n++)
+    {
+        for (j = 0; j <= 59 ; j++)
+        {    
+            i = n / 10;
+            k = n % 10;
+            o = j / 10;
+            p = j % 10;
+            _putchar(i + '0');
+            _putchar(k + '0');
+            _putchar(':');
+            _putchar(o + '0');
+            _putchar(p + '0');
+            _putchar('\n');
+        }
+        
+    }
+}
