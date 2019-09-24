73f42f2ae484bc77d53a041ba55b35e64ee04851
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2019-09-24 19:29:45 +0000

    add 9 times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,1 +1,35 @@
 #include "holberton.h"
+/**
+ * times_table - writes the character c to stdout
+ *
+ * On error, -1 is returned, and errno is set appropriately.
+*/
+void times_table(void)
+{
+int a, b, c;
+for (a = 0; a <= 9; a++)
+{
+for (b = 0; b <= 9; b++)
+{
+c = a * b;
+if (c < 10)
+{
+if (b > 0)
+{
+_putchar(',');
+_putchar(' ');
+_putchar(' ');
+}
+_putchar(c + '0');
+}
+else
+{
+_putchar(',');
+_putchar(' ');
+_putchar(c / 10 + '0');
+_putchar(c % 10 + '0');
+}
+}
+_putchar('\n');
+}
+}
