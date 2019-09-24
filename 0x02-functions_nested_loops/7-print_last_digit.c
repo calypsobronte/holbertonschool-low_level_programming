5a344ccc359dae30a14c632de034bfa7768ff281
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2019-09-24 18:26:14 +0000

    add 7-print-last

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,1 +1,24 @@
 #include "holberton.h"
+/**
+ * print_last_digit - writes the character c to stdout
+ * @n: The character to print
+ *
+ * Return: 1 or 0
+ * On error, -1 is returned, and errno is set appropriately.
+*/
+int print_last_digit(int n)
+{
+int ret;
+
+if (n > 0)
+{
+ret = n % 10;
+_putchar(ret + '0');
+}
+else
+{
+ret = (n * -1) % 10;
+_putchar(ret + '0');
+}
+return (ret);
+}
