269da9c4b868128b7ef87d24666bc0962c615360
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2019-09-24 18:10:02 +0000

    add 5-print_sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,1 +1,26 @@
 #include "holberton.h"
+/**
+ * print_sign - writes the character c to stdout
+ * @n: The character to print
+ *
+ * Return: 1 or 0
+ * On error, -1 is returned, and errno is set appropriately.
+*/
+int print_sign(int n)
+{
+if (n > 0)
+{
+    _putchar('+');
+    return (1);
+}
+else if (n == 0)
+{
+    _putchar('0');
+    return (0);
+}
+else
+{
+    _putchar('-');
+    return (-1);
+}
+}
