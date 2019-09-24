f0b40097a9c591e31cb370840b0c1bdf954b4f00
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2019-09-24 19:41:40 +0000

    add 11 print to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,1 +1,34 @@
 #include "holberton.h"
+#include <stdio.h>
+/**
+ * print_to_98 - writes the character c to stdout
+ * @n: The character to print
+ *
+ * On error, -1 is returned, and errno is set appropriately.
+*/
+void print_to_98(int n)
+{
+if (n < 98)
+{
+for (n = n; n <= 98; n++)
+{
+printf("%d", n);
+if (n != 98)
+{
+printf(", ");
+}
+}
+}
+else
+{
+for (n = n; n >= 98; n--)
+{
+printf("%d", n);
+if (n != 98)
+{
+printf(", ");
+}
+}
+}
+printf("\n");
+}
