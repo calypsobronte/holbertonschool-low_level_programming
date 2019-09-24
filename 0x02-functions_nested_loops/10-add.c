1de8f9ec06aa5f1279354f9364e1121d5a66af58
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2019-09-24 19:36:26 +0000

    add 10 add sum

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,1 +1,13 @@
 #include "holberton.h"
+/**
+ * add - writes the character c to stdout
+ * @a: The character to print
+ * @b: The character to print
+ *
+ * Return: a + b
+ * On error, -1 is returned, and errno is set appropriately.
+*/
+int add(int a, int b)
+{
+    return (a + b);
+}
