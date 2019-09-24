40f9f083f2a50329d8b50e8b312404dbe83db079
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2019-09-24 18:18:08 +0000

    add 6-abs

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,1 +1,19 @@
 #include "holberton.h"
+/**
+ * _abs - writes the character c to stdout
+ * @n: The character to print
+ *
+ * Return: 0
+ * On error, -1 is returned, and errno is set appropriately.
+*/
+int _abs(int n)
+{
+if (n >= 0)
+{
+return (n);
+}
+else
+{
+return (n * -1);
+}
+}
