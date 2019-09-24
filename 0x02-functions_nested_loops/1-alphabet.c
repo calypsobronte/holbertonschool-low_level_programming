4173076c73ab52ee7d24ce2034a8cc3aabd7a604
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2019-09-24 05:20:28 +0000

    update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,18 +1,13 @@
 #include "holberton.h"
-
 /**
  * main - empty file
  *
  * Return: ALways 0 (Success)
  */
-
 void print_alphabet(void)
 {
-	int i;
-
-	for (i = 'a'; i <= 'z'; i++)
-	{
-		_putchar(i);
-	}
-	_putchar('\n');
+int c;
+for (c = 'a'; c <= 'z'; c++)
+_putchar(c);
+_putchar('\n');
 }
