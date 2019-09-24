3bf1c1163f4111cca52368b1a1d502686e2df28c
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2019-09-24 04:13:18 +0000

    11. Holberton School, 98 Battery Street, the OG 10. a + b

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,1 +1,34 @@
-#calypsobronte
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * 
+ * 
+ * 
+ */
+void print_to_98(int n)
+{
+    if (n < 98)
+    {
+        for (n = n; n <= 98; n++)
+        {
+            printf("%d", n);
+            if (n != 98)
+            {
+                printf(", ");
+            }
+        }
+    }
+    else
+    {
+        for (n = n; n >= 98; n--)
+        {
+            printf("%d", n);
+            if (n != 98)
+            {
+                printf(", ");
+            }
+        }
+    }
+    printf("\n");
+}
