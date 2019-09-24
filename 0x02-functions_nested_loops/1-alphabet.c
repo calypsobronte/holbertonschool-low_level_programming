5fff31e9e3d815189f07f0a2b3022b461e791484
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2019-09-24 05:40:52 +0000

    update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,13 +2,13 @@
 /**
  * print_alphabet - print the alphabet
  *
- * Return: 0
+ * Return: Alwys 0 (Success)
  */
 void print_alphabet(void)
 {
-int i;
-
-for (i = 'a'; i <= 'z'; i++)
-_putchar(i);
-_putchar('\n');
+	int i;
+	for (i = 'a'; i <= 'z'; i++)
+		_putchar(i);
+	_putchar('\n');
 }
+
