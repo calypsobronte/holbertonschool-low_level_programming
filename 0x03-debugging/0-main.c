f3cb79a83fc1bd8c32877490f9dbe3fcbbf0c3da
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2019-09-24 22:50:18 +0000

    add 0-main

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,9 +1,9 @@
 #include "holberton.h"
 
 /**
-* main - tests function that prints if integer is positive or negative
-* Return: 0
-*/
+ * main - tests function that prints if integer is positive or negative
+ * Return: 0
+ */
 
 int main(void)
 {
@@ -14,3 +14,4 @@ positive_or_negative(i);
 
 return (0);
 }
+
