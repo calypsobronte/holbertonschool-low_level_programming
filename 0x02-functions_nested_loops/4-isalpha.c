fc727b737d914efbdb0b89002e3cc48d68573fbe
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2019-09-24 18:07:23 +0000

    add 4-isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,1 +1,22 @@
 #include "holberton.h"
+/**
+ * _isalpha - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: 1 or 0
+ * On error, -1 is returned, and errno is set appropriately.
+*/
+int _isalpha(int c)
+{
+
+if ((c >= 'a' && c <= 'z') || (c > 'A' && c < 'Z'))
+{
+return (1);
+_putchar('\n');
+}
+else
+{
+return (0);
+_putchar('\n');
+}
+}
