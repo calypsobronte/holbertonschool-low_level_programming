c14d9ecf3600295e4c1a9505c0fd9758ce41891f
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2019-09-24 16:21:10 +0000

    add 2-print

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,1 +1,19 @@
 #include "holberton.h"
+/**
+ * print_alphabet_x10 - writes the character c to stdout
+ *
+ * On error, -1 is returned, and errno is set appropriately.
+*/
+void print_alphabet_x10(void)
+{
+int c, n;
+
+for (n = 0; n < 9; n++)
+{
+for (c = 'a'; c <= 'z'; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+}
+}
