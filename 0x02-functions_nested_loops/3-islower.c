608efe3c2d45a75a2f64f6a98c6bc0c6468fa1f7
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2019-09-24 17:57:12 +0000

    add 3-islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,1 +1,19 @@
 #include "holberton.h"
+/**
+ * print_alphabet_x10 - writes the character c to stdout
+ *
+ * Return: 1 or 0
+ * On error, -1 is returned, and errno is set appropriately.
+*/
+int _islower(int c)
+{
+
+if (c > 'a' && c < 'z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
