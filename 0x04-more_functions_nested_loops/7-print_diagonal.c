72fe012699fa4407626243726a79cc33985fca03
Lina María Montaño Ramírez <18755556+calypsobronte@users.noreply.github.com>
2019-09-26 16:36:52 -0500

    add 7-print_diagonal.c

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+/**
+* print_diagonal - void
+* @n: print
+*
+* On error, -1 is returned, and errno is set appropriately.
+*/
+void print_diagonal(int n)
+{
+int a, b;
+if (n > 0)
+{
+for (a = 1; a <= n; a++)
+{
+for (b = 1; b < a; b++)
+{
+_putchar(' ');
+}
+_putchar(92);
+_putchar('\n');
+}
+}
+else
+{
+_putchar('\n');
+}
+}
