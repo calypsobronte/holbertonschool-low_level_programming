cae191c9c6aaa6ea2bc2e9368d7452041b6bf248
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2019-09-26 15:24:35 +0000

    2. Collaboration is multiplication

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,1 +1,15 @@
 #include "holberton.h"
+/**
+ * mul - writes the character c to stdout
+ * @a: The character to print
+ * @b: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int mul(int a, int b)
+{
+int z;
+z = a * b;
+return (z);
+}
--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -1,3 +1,4 @@
 /* Library Holberton.h */
 int _isupper(int c);
 int _isdigit(int c);
+int mul(int a, int b);
