874b2f4fc14e0596b334751c4453967606f3ce27
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2019-09-26 20:00:29 +0000

    5. Numbers constitute the only universal language

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,1 +1,20 @@
 #include "holberton.h"
+/**
+ * more_numbers - void
+ *
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+void more_numbers(void)
+{
+int a, b;
+for (a = 0; a <= 9; a++)
+{
+for (b = 0; b <= 14; b++)
+{
+if (b > 9)
+_putchar((b / 10) + '0');
+_putchar((b % 10) + '0');
+}
+_putchar('\n');
+}
+}
