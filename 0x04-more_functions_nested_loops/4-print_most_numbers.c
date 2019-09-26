e11615b0dea1a8c112fa50122bfdc7f5e3f17c1c
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2019-09-26 16:12:50 +0000

    4. I believe in numbers and signs

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,1 +1,14 @@
 #include "holberton.h"
+/**
+ * print_most_numbers - void
+ *
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+void print_most_numbers(void)
+{
+int a;
+for (a = '0'; a <= '9'; a++)
+if (a != '2' && a != '4')
+_putchar(a);
+_putchar('\n');
+}
