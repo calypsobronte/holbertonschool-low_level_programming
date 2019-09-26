663fde9876cb53ada289122110c18e045f13763f
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2019-09-26 22:18:50 +0000

    8. You are so much sunshine in every square inch

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,9 +1,8 @@
 #include "holberton.h"
 /**
- * _isupper - writes the character c to stdout
- * @c: The character to print
+ * print_square - writes the character c to stdout
+ * @size: The character to print
  *
- * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
 void print_square(int size)
