f23ef864d12f5cc1d92a1ea2d6bbb0bc698cbaf7
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2019-09-27 04:36:37 +0000

    12. Numbers have life; they're not just symbols on paper

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,1 +1,27 @@
 #include "holberton.h"
+/**
+ * print_number - writes the character c to stdout
+ * @n: The character to print
+ *
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+void print_number(int n)
+{
+if (n > -10 && n < 10)
+{
+if (n < 0)
+{
+_putchar('-');
+n = n * -1;
+}
+_putchar(n + '0');
+}
+else
+{
+print_number(n / 10);
+n = n % 10;
+if (n < 0)
+n = n * -1;
+_putchar(n + '0');
+}
+}
