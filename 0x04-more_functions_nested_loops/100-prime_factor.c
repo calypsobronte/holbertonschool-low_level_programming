3d359102e5906ebffe9bf27d3de394696a449841
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2019-09-27 04:08:57 +0000

    Fix task 100

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - check the code for Holberton School students.
+ * main - main.
  *
  * Return: Always 0.
  */
@@ -8,8 +8,10 @@ int main(void)
 {
 long a, b = 612852475143;
 for (a = 2; a < b; a++)
+{
 if (b % a == 0)
 b = b / a;
+}
 printf("%ld", b);
 return (0);
 }
