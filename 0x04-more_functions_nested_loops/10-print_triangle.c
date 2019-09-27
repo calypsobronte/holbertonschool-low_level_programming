e939542a99723b7baffc38668c523c0c009700f1
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2019-09-27 00:21:38 +0000

    10. Triangles

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -1,1 +1,27 @@
 #include "holberton.h"
+/**
+* print_triangle - void
+* @size: print
+*
+* On error, -1 is returned, and errno is set appropriately.
+*/
+void print_triangle(int size)
+{
+int a, b;
+if (size > 0)
+{
+for (a = 1; a <= size; a++)
+{
+for (b = size; b >= 1 ; b--)
+{
+if (a < b)
+_putchar(' ');
+else
+_putchar(35);
+}
+_putchar('\n');
+}
+}
+else
+_putchar('\n');
+}
