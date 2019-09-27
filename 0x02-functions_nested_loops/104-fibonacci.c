d53a6fa9c3ecdb109309ed15e97ac19ebdd72da6
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2019-09-27 05:15:25 +0000

    16. In computer class, the first assignment was to write a program to print the first 100 Fibonacci numbers. Instead, I wrote a program that would steal passwords of students. My teacher gave me an A+

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
-
 /**
  * main - prints
  *
  * Return: Always 0;
  */
-
-int  main(void)
+int main(void)
 {
 int i;
 float x, y, z;
@@ -18,10 +16,10 @@ printf("%.0f, ", x);
 printf("%.0f", y);
 for (i = 0; i < 96; i++)
 {
-    z = x + y;
-    printf(", %.0f", z);
-    x = y;
-    y = z;
+z = x + y;
+printf(", %.0f", z);
+x = y;
+y = z;
 }
 putchar('\n');
 return (0);
