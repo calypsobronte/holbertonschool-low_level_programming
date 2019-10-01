eb2bd6349c716388934b1759afd56732ee773250
Lina María Montaño Ramírez - calypsobronte <codelinamaria@gmail.com>
2019-10-01 20:43:38 +0000

    3. I do not fear computers. I fear the lack of them

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,1 +1,12 @@
 #include "holberton.h"
+/**
+ * _puts - prints string
+ *@str: character letter
+*/
+void _puts(char *str)
+{
+int a;
+for (a = 0; str[a] != 0; a++)
+_putchar(str[a]);
+_putchar('\n');
+}
