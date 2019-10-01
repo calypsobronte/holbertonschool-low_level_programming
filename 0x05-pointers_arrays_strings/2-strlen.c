68895913d20ae255cffeae5ed6b865b39f5d735c
Lina María Montaño Ramírez - calypsobronte <codelinamaria@gmail.com>
2019-10-01 20:10:28 +0000

    2. This report, by its very length, defends itself against the risk of being read

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,1 +1,13 @@
 #include "holberton.h"
+/**
+ * _strlen - character
+ * @s: - letter
+ * Return: a
+ */
+int _strlen(char *s)
+{
+int a;
+while (s[a] != 0)
+a++;
+return (a);
+}
--- 0x05-pointers_arrays_strings/holberton.h
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -1,3 +1,4 @@
 /* Library Holberton.h */
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
+int _strlen(char *s);
