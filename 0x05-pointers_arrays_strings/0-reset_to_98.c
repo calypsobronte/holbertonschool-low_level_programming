09ce6b0de2f2252255e27a6c7b1393216ff20581
Lina María Montaño Ramírez - calypsobronte <codelinamaria@gmail.com>
2019-10-01 03:17:31 +0000

    0. 98 Battery st

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,1 +1,9 @@
 #include "holberton.h"
+/**
+ * reset_to_98 - description
+ * @n:  value
+ */
+void reset_to_98(int *n)
+{
+*n=98;
+}
--- 0x05-pointers_arrays_strings/holberton.h
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -1,1 +1,3 @@
 /* Library Holberton.h */
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
