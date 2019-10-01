07ac933a8541ed34f99a31a68e919a5dc4d03d3f
Lina María Montaño Ramírez - calypsobronte <codelinamaria@gmail.com>
2019-10-01 21:25:44 +0000

    4. I can only go one way. I've not got a reverse gear

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,1 +1,15 @@
 #include "holberton.h"
+/**
+ * print_rev - prints string
+ *@str: character letter
+ */
+void print_rev(char *str)
+{
+int a;
+char b;
+for (a = 0; str[a] != 0; a++)
+for (a = a - 1; a >= 0; a--)
+b = str[a];
+_putchar(b);
+_putchar('\n');
+}
