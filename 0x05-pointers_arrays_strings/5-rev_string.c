79a4066564644233f96f9105b3ce224339145d3d
Lina María Montaño Ramírez - calypsobronte <codelinamaria@gmail.com>
2019-10-02 04:15:25 +0000

    fix task 5  rev string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,1 +1,19 @@
 #include "holberton.h"
+/**
+ *rev_string - h
+ *@s: letter
+ */
+void rev_string(char *s)
+{
+int a, b = 0;
+char c;
+while (s[b] != 0)
+b++;
+b--;
+for (a = 0; a < b; b--, a++)
+{
+c = s[a];
+s[a] = s[b];
+s[b] = c;
+}
+}
