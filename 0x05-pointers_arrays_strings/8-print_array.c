77c2cbad1e0dcd9cf8f74a2bba151af0dd4d6c29
Lina María Montaño Ramírez - calypsobronte <codelinamaria@gmail.com>
2019-10-02 04:57:23 +0000

    8. Arrays are not pointers

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,1 +1,24 @@
 #include "holberton.h"
+#include <stdio.h>
+/**
+ * print_array - print
+ * @a: letter
+ * @n: elements
+ */
+void print_array(int *a, int n)
+{
+int b;
+
+for (b = 0; b < n; b++)
+{
+if (b < n - 1)
+{
+printf("%d, ", a[b]);
+}
+else
+{
+printf("%d", a[b]);
+}
+}
+printf("\n");
+}
