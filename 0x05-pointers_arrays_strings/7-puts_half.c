bb67acfaff11f0458c0d02ba0b31219f8144d73f
Lina María Montaño Ramírez - calypsobronte <codelinamaria@gmail.com>
2019-10-02 04:51:10 +0000

    7. Winning is only half of it. Having fun is the other ha

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,1 +1,27 @@
 #include "holberton.h"
+/**
+ * puts_half - print
+ * @str: string
+ */
+void puts_half(char *str)
+{
+int a, length_of_the_string = 0;
+while (str[length_of_the_string] != 0)
+{
+length_of_the_string++;
+}
+if (length_of_the_string % 2 != 0)
+{
+a = (length_of_the_string + 1) / 2;
+}
+else
+{
+a = length_of_the_string / 2;
+}
+while (a < length_of_the_string)
+{
+_putchar(str[a]);
+a++;
+}
+_putchar('\n');
+}
