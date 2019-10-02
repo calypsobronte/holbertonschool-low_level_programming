f1583a553712763de6017f45d9d72960650d5878
Lina María Montaño Ramírez - calypsobronte <codelinamaria@gmail.com>
2019-10-02 05:03:51 +0000

    9. strcpy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,1 +1,19 @@
 #include "holberton.h"
+/**
+ * _strcpy - copues
+ * @dest:
+ * @src:
+ * Return: dest a copy
+ */
+char *_strcpy(char *dest, char *src)
+{
+int a = 0;
+
+while (*src != 0)
+{
+dest[a] = *src;
+a++;
+src++;
+}
+return (dest);
+}
