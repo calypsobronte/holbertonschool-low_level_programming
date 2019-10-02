c80809ac12e950f63e77c89c705b2acea223c46a
Lina María Montaño Ramírez - calypsobronte <codelinamaria@gmail.com>
2019-10-02 04:30:03 +0000

    6. Half the lies they tell about me aren't true

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,1 +1,13 @@
 #include "holberton.h"
+/**
+ * puts2 - 2
+ * @str: string
+ */
+void puts2(char *str)
+{
+int a;
+for (a = 0; str[a] != 0; a++)
+if (a % 2 == 0)
+_putchar(str[a]);
+_putchar('\n');
+}
