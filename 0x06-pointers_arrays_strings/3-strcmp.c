d03d7a6343d73939dbbbb95f5a43e9e5ddc7b6dc
Lina María Montaño Ramírez - calypsobronte <codelinamaria@gmail.com>
2019-10-03 20:44:28 +0000

    3. strcmp fix

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,13 +1,14 @@
 #include "holberton.h"
 /**
- * _strcmp - description
- * @s1: descript
- * @s2: descript
- * Return: j
- */
+* _strcmp - description
+* @s1: descript
+* @s2: descript
+* Return: j
+*/
 int _strcmp(char *s1, char *s2)
 {
 int a = 0, b, c, d;
+
 while (s1[a] == s2[a] && s1[a] != 0 && s2[a] != 0)
 a++;
 c = s1[a] + '0';
@@ -15,3 +16,4 @@ d = s2[a] + '0';
 b = c - d;
 return (b);
 }
+
