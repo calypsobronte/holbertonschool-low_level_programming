70e475e16d79d97ae36d612aab086ae3fea523ac
Lina María Montaño Ramírez - calypsobronte <codelinamaria@gmail.com>
2019-10-03 20:26:51 +0000

    3. strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,9 +7,13 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-int i = 0, j;
+int i, j;
+
+i = 0;
 while (s1[i] == s2[i] && s1[i] != 0 && s2[i] != 0)
+{
 i++;
+}
 int a = (s1[i] + '0');
 int b = (s2[i] + '0');
 j = a - b;
