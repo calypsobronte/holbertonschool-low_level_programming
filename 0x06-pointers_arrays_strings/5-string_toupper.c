64c4ec4ee7338b7c1fd4000a75fd2a917753e5ba
Lina María Montaño Ramírez - calypsobronte <codelinamaria@gmail.com>
2019-10-03 22:33:31 +0000

    5. Always look up

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,1 +1,15 @@
-#include "holberton.h
+#include "holberton.h"
+char *string_toupper(char *s)
+{
+int i;
+//for (i = 0; s[i] != 0; i++)
+while (s[i] != '\0')
+{
+i++;
+if (s[i] > 'a' && s[i] < 'z')
+{
+s[i] -= 32;
+}
+}
+return (s);
+}
