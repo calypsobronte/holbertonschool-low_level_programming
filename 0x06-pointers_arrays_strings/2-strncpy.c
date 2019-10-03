b17b3649e6f54958b0195048f24076ddefddbaaf
Lina María Montaño Ramírez - calypsobronte <codelinamaria@gmail.com>
2019-10-03 19:16:55 +0000

    2. strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,1 +1,17 @@
-#include "holberton.h
+#include "holberton.h"
+/**
+ * _strncpy -  description
+ * @dest: ///
+ * @src: ---
+ * @n: ===
+ * Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+int b, i;
+for (b = 0; b < n && src[b] != 0; b++)
+dest[b] = src[b];
+for (i = b; i < n; i++)
+dest[i] = 0;
+return (dest);
+}
--- 0x06-pointers_arrays_strings/holberton.h
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -1,3 +1,4 @@
 /* Library holberton */
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
