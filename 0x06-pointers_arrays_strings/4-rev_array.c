286dbd05a89064d46520b815f0adf46efa0a4e49
Lina María Montaño Ramírez - calypsobronte <codelinamaria@gmail.com>
2019-10-03 21:49:58 +0000

    4. I am a kind of paranoid in reverse. I suspect people of plotting to make me happy

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,1 +1,16 @@
-#include "holberton.h
+#include "holberton.h"
+/**
+ * reverse_array - description
+ * @a: descript
+ * @n: descript
+ */
+void reverse_array(int *a, int n)
+{
+int b, temp, c;
+n = n - 1;
+for (b = n; b > (n / 2); b--)
+temp = a[b];
+c = n - b;
+a[b] = a[c];
+a[c] = temp;
+}
