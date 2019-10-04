0e3e50289ead888b67f8b238a5f54c76fd4d5e24
Lina María Montaño Ramírez - calypsobronte <codelinamaria@gmail.com>
2019-10-04 05:03:13 +0000

    9. Numbers have life; they're not just symbols on pape

--- 0x06-pointers_arrays_strings/100-print_number.c
+++ 0x06-pointers_arrays_strings/100-print_number.c
@@ -1,1 +1,22 @@
-#include "holberton.h
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * print_number - description
+ * @n: descript
+ * Return: -
+ */
+void print_number(int n)
+{
+int a = n;
+char b = 48;
+if (a / 10 == 0)
+{
+if (a < 0)
+_putchar(45);
+_putchar(abs(a) + b);
+return;
+}
+print_number(a / 10);
+b = b + (abs(a % 10));
+_putchar(b);
+}
