b82afd0fbff896687688784fd9b7fa89a8899a37
Lina María Montaño Ramírez - calypsobronte <codelinamaria@gmail.com>
2019-10-04 05:09:51 +0000

    una parte de challange de 102

--- 0x06-pointers_arrays_strings/102-infinite_add.c
+++ 0x06-pointers_arrays_strings/102-infinite_add.c
@@ -1,1 +1,13 @@
-#include "holberton.h
+#include "holberton.h"
+/**
+ * infinite_add - string
+ * @n1: letter
+ * @n2: letter
+ * @r: letter
+ * @size_r: letter
+ * Return: s
+ */
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+
+}
