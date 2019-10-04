076ca4c1f09f2b720452963f7b38df46f6e031a5
Lina María Montaño Ramírez - calypsobronte <codelinamaria@gmail.com>
2019-10-04 03:58:09 +0000

    Fix 8. rot13

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -10,18 +10,16 @@ char *rot13(char *s)
 {
 int i;
 i = 0;
-while (s[i] != '\0')
+while (s[i] != 0)
 {
 if ((s[i] >= 'a' && s[i] <= 'm') || (s[i] >= 'A' && s[i] <= 'M'))
 {
 s[i] = (s[i] + 13);
 }
 else
-{
 while ((s[i] >= 'n' && s[i] <= 'z') || (s[i] >= 'N' && s[i] <= 'Z'))
 {
 s[i] = (s[i] - 13);
-}
 }
 i++;
 }
