963944c545f4c03d8a410b7f77e52c542c64380b
Lina María Montaño Ramírez - calypsobronte <codelinamaria@gmail.com>
2019-10-04 03:44:48 +0000

    8. rot13

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -1,1 +1,26 @@
-#include "holberton.h
+#include "holberton.h"
+/**
+ * rot13 - conver string o rot13 - //abcdefghijklmABCDEFGHIJKLM += 13 - //nopqrstuvwzyzNOPQRSTUVWXYZ = -13
+ * @s: letter
+ * Return: s
+ */
+char *rot13(char *s)
+{
+int i;
+i = 0;
+while (s[i] != '\0')
+{
+
+if ((s[i] >= 'a' && s[i] <= 'm') || (s[i] >= 'A' && s[i] <= 'M'))
+{
+s[i] = (s[i] + 13);
+}
+else
+while ((s[i] >= 'n' && s[i] <= 'z') || (s[i] >= 'N' && s[i] <= 'Z'))
+{
+s[i] = (s[i] - 13);
+}
+i++;
+}
+return (s);
+}
