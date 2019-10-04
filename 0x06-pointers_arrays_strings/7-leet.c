bb1f25e090f1223d5fe30320a56f8211ace5dbde
Lina María Montaño Ramírez - calypsobronte <codelinamaria@gmail.com>
2019-10-04 01:03:47 +0000

    solution of task 7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -7,13 +7,13 @@
 char *leet(char *s)
 {
 int a = 0, b;
-char c[] = "aAeEoOtTlT";
-char d[] = "4433007711";
+char c[] = "aeotl";
+char d[] = "43071";
 while (s[a] != 0)
 {
 for (b = 0; b < 6; b++)
 {
-if (s[a] == c[b] || s[a] == c[b + 1])
+if (s[a] == c[b] || s[a] + 32 == c[b + 1])
 {
 s[a] = d[b];
 }
