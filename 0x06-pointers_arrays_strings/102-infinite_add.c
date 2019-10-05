83bb54603f7488d8051545ae33569a7f7de1685e
Lina María Montaño Ramírez - calypsobronte <codelinamaria@gmail.com>
2019-10-05 22:10:13 +0000

    11. It is the addition of strangeness to beauty that constitutes the romantic character in art

--- 0x06-pointers_arrays_strings/102-infinite_add.c
+++ 0x06-pointers_arrays_strings/102-infinite_add.c
@@ -9,5 +9,37 @@
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-
+int a, b, c, d, e, f = 0;
+for (a = 0; n1[a] != '\0'; a++)
+{}
+for (b = 0; n2[b] != '\0'; b++)
+{}
+if (a > size_r || b > size_r)
+return (0);
+a--;
+b--;
+for (c = 0; c < size_r - 1; a--, b--, c++)
+{
+e = f;
+if (a >= 0)
+e += n1[a] - '0';
+if (b >= 0)
+e += n2[b] - '0';
+if (a < 0 && b < 0 && e == 0)
+{
+break;
+}
+f = e / 10;
+r[c] = e % 10 + '0';
+}
+r[c] = '\0';
+if (a >= 0 || b >= 0 || f)
+return (0);
+for (c -= 1, d = 0; d < c; c--, d++)
+{
+f = r[c];
+r[c] = r[d];
+r[d] = f;
+}
+return (r);
 }
