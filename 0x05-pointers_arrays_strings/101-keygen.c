72ddee82aa788dae0cbdedef78dad45d1af75c25
Lina María Montaño Ramírez - calypsobronte <codelinamaria@gmail.com>
2019-10-05 21:26:00 +0000

    11. Don't hate the hacker, hate the code

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,1 +1,23 @@
-#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - a
+ * Return: 0
+*/
+int main(void)
+{
+char a = 0, b = 0;
+int c = 0;
+srand(time(0));
+while (c <= 2645)
+{
+a = rand() % 128;
+c += a;
+putchar(a);
+}
+b = 2772 - c;
+putchar(b);
+return (0);
+}
