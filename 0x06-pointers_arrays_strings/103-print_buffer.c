2fc2feea16f1afcb181657dfd6057bf324ac9275
Lina María Montaño Ramírez - calypsobronte <codelinamaria@gmail.com>
2019-10-06 21:15:25 +0000

    Fix 103

--- 0x06-pointers_arrays_strings/103-print_buffer.c
+++ 0x06-pointers_arrays_strings/103-print_buffer.c
@@ -7,11 +7,18 @@
 */
 void print_buffer(char *b, int size)
 {
-int a;
+int a, c;
 if (size <= 0)
 printf("\n");
 else
 for (a = 0; a < size; a += 10)
-printf("%08x: ", b[a]);
+printf("%08x: ", a);
+for (c = 0; c < 10; a++)
+d = c + a;
+if (d < size)
+e = a + c;
+printf("%02x: ", b[e]);
+else
+printf(" ");
 printf("\n");
 }
