bb9b5abe53cd382a677ba3e502f16da9d146a7ac
Lina María Montaño Ramírez - calypsobronte <codelinamaria@gmail.com>
2019-10-07 21:49:46 +0000

    5. strstr

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,1 +1,22 @@
 #include "holberton.h"
+/**
+ * _strpbrk =
+ * @s: cc
+ * @accept: cc
+ * Return: s + c
+ */
+char *_strpbrk(char *s, char *accept)
+{
+unsigned int a, b, c = 1, band;
+for (a = 0; s[a] != '\0'; a++)
+{
+for (b = 0; accept[b] != '\0'; b++)
+if (s[a] == accept[b] || band == 0)
+band = 0;
+else
+band = 1;
+}
+if (band == 0)
+c++;
+return (s + c);
+}
