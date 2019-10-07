62b33bf64d52e220f2e8791e5eb9ce1ec13f3219
Lina María Montaño Ramírez - calypsobronte <codelinamaria@gmail.com>
2019-10-07 20:30:03 +0000

    3. strspn

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,1 +1,16 @@
 #include "holberton.h"
+/**
+ * _strspn - comparacion entre cada posicion
+ * @s: point
+ * @accept: point
+ * Return: c
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+unsigned int a, b, c = 0;
+for (a = 0; s[a] != 32; a++)
+for (b = 0; accept[b] != 0; b++)
+if (s[a] == accept[b])
+c++;
+return (c);
+}
