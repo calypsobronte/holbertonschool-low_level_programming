0e9d1fe31d3d4698c94500f81090d8a7051b4d78
Lina María Montaño Ramírez - calypsobronte <codelinamaria@gmail.com>
2019-10-07 23:49:34 +0000

    6. Chess is mental torture

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,1 +1,22 @@
 #include "holberton.h"
+/**
+ * print_chessboard  - print string
+ * @a: point  in 8
+ */
+void print_chessboard(char (*a)[8])
+{
+int b, c;
+b = 0;
+while (b < 8)
+{
+c = 0;
+while (c < 8)
+{
+_putchar(a[b][c]);
+if (c == 7)
+_putchar('\n');
+c++;
+}
+b++;
+}
+}
