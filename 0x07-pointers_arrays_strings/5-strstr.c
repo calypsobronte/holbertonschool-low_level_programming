276d0ac4cc59582bfbf51d63d3656a6645c8cbe2
Lina María Montaño Ramírez - calypsobronte <codelinamaria@gmail.com>
2019-10-07 22:27:22 +0000

    5. strstr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,1 +1,18 @@
 #include "holberton.h"
+/**
+ * _strstr - ho
+ * @haystack: cc
+ * @needle: cc
+ * Return: s + c
+ */
+char *_strstr(char *haystack, char *needle)
+{
+int a, b, c = 0;
+for (a = 0; haystack[a] != '\0'; a++)
+{
+for (b = 0; needle[b] != '\0'; b++)
+if (haystack[a] == needle[b])
+c++;
+}
+return (haystack + (c - 1));
+}
