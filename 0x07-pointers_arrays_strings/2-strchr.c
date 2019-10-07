438139c9d2bad6ca34a0f21b09ed0d2b935e76a2
Lina María Montaño Ramírez - calypsobronte <codelinamaria@gmail.com>
2019-10-07 19:52:52 +0000

    2. strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,1 +1,17 @@
 #include "holberton.h"
+/**
+ * _strchr - print
+ * @s: string
+ * @c: string
+ */
+char *_strchr(char *s, char c)
+{
+unsigned int a = 0;
+while (s[a] != 0)
+{
+if (s[a] == c)
+return (s + a);
+a++;
+}
+return (0);
+}
