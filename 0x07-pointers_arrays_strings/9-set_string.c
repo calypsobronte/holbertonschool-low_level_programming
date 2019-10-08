a926ae3bd52b1f272e6fe84643701e643fe80f93
Lina María Montaño Ramírez - calypsobronte <codelinamaria@gmail.com>
2019-10-08 02:35:56 +0000

    8. Double pointer, double fun

--- 0x07-pointers_arrays_strings/9-set_string.c
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -1,1 +1,10 @@
 #include "holberton.h"
+/**
+ * set_string - evalua un pointer de otro pointer
+ * @s: point direccion a otro point
+ * @to: string destiono
+ */
+void set_string(char **s, char *to)
+{
+*s = to;
+}
