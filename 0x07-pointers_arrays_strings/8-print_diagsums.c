c74079ba423b047ce51d9d607e78318b6c4f3caf
Lina María Montaño Ramírez - calypsobronte <codelinamaria@gmail.com>
2019-10-08 02:28:06 +0000

    7. The line of life is a ragged diagonal between duty and desire

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,1 +1,16 @@
 #include "holberton.h"
+#include <stdio.h>
+/**
+ * print_diagsums - imprimir la suma de dos diagonales de una matriz cuadrada
+ * @a: pointer
+ * @size: tama;o
+ */
+void print_diagsums(int *a, int size)
+{
+int b, c, d = 0, e = 0;
+for (b = 0; b < size; b++)
+d += a[(size + 1) * b];
+for (c = 0; c < size; c++)
+e += a[(size - 1) * (c + 1)];
+printf("%d, %d\n", d, e);
+}
