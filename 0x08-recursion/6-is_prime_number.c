b3320709793f25cdb97a5224b3f8cdfe681b8433
Lina María Montaño Ramírez - calypsobronte <codelinamaria@gmail.com>
2019-10-09 22:00:36 +0000

    6. Inception. Is it possible?

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,1 +1,35 @@
 #include "holberton.h"
+/**
+ * prime_recu - print
+ * @n: piont
+ * @i: piont
+ * Return: -1
+ */
+int prime_recu(int n, int i)
+{
+if (i == n)
+{
+return (1);
+}
+if (i % n == 0)
+{
+return (0);
+}
+return (prime_recu(n + 1, i));
+}
+
+/**
+ * is_prime_number - print
+ * @n: piont
+ * Return: -1
+ */
+
+int is_prime_number(int n)
+{
+int i = 2;
+if (n == 1 || n < 0)
+{
+return (0);
+}
+return (prime_recu(i, n));
+}
