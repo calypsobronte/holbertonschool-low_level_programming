eeb81e7d316874b91ccb1ee7160ac3d1aff07a87
Lina María Montaño Ramírez - calypsobronte <codelinamaria@gmail.com>
2019-10-09 18:32:07 +0000

    4. Once an idea has taken hold of the brain it's almost impossible to eradicate

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,1 +1,17 @@
 #include "holberton.h"
+/**
+ * _pow_recursion - print
+ * @n: piont
+ * Return: -1
+ */
+int _pow_recursion(int x, int y)
+{
+if (y < 0)
+{
+return (-1);
+}
+else if (y == 0)
+return (1);
+else
+return (x * _pow_recursion(x, y - 1));
+}
