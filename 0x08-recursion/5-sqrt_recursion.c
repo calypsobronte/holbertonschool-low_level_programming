4398a5735df49712e4441c9e9ed78778939f2e7f
Lina María Montaño Ramírez - calypsobronte <codelinamaria@gmail.com>
2019-10-09 20:34:22 +0000

    5. Your subconscious is looking for the dreamer

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,1 +1,31 @@
 #include "holberton.h"
+/**
+ * sqrt_recu - print
+ * @n: piont
+ * @i: piont
+ * Return: -1
+ */
+int sqrt_recu(int n, int i)
+{
+if (i * i > n)
+{
+return (-1);
+}
+if (i * i == n)
+{
+return (i);
+}
+return (sqrt_recu(n, i + 1));
+}
+
+/**
+ * _sqrt_recursion - print
+ * @n: piont
+ * Return: -1
+ */
+
+int _sqrt_recursion(int n)
+{
+int i = 0;
+return (sqrt_recu(n, i));
+}
