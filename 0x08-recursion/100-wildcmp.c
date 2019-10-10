8d224e6102226a1a4d49533735ee120017408fd0
Lina María Montaño Ramírez - calypsobronte <codelinamaria@gmail.com>
2019-10-10 04:43:10 +0000

    8. Inception. Now, before you bother telling me it's impossible.

--- 0x08-recursion/100-wildcmp.c
+++ 0x08-recursion/100-wildcmp.c
@@ -14,6 +14,8 @@ return (wildcmp(s1 + 1, s2 + 1));
 if (*s2 == '*' && (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2)))
 return (1);
 if (*s2 == '*' && *(s1 + 1) != '\0' && *s2 == '\0')
+{
 return (0);
+}
 return (0);
 }
