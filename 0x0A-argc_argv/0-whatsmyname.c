f28bc609f40a4a7c90266907bed43f5aea532739
Lina María Montaño Ramírez - calypsobronte <codelinamaria@gmail.com>
2019-10-13 23:14:54 +0000

    0. It ain't what they call you, it's what you answer to

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - name
+ * @argc: var inte
+ * @argv: pointer string
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+argc = argc;
+printf("%s\n", *argv);
+return (0);
+}
