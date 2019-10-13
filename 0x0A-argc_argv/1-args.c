df1ba084cacfc104b82ca87c1ce98a4554c6ffb7
Lina María Montaño Ramírez - calypsobronte <codelinamaria@gmail.com>
2019-10-13 23:24:44 +0000

    1. Silence is argument carried out by other means

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - name
+ * @argc: var inte
+ * @argv: pointer string
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+argv = argv;
+printf("%d\n", argc - 1);
+return (0);
+}
