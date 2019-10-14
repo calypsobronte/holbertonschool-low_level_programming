2e92c5b3c8a01c1421e90ced0ced55bebd035dd4
Lina María Montaño Ramírez - calypsobronte <codelinamaria@gmail.com>
2019-10-14 00:05:44 +0000

    3. Neither irony nor sarcasm is argument

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - mul of two numbers atoi: Converts a string to its numeric value (integer)
+ * @argc: integer
+ * @argv: point string
+ * Return: 0 or 1
+ */
+int main(int argc, char *argv[])
+{
+int a;
+if (argc == 3)
+{
+a = atoi(argv[1]) * atoi(argv[2]);
+printf("%d\n", a);
+return (0);
+}
+else
+printf("Error\n");
+return (1);
+}
