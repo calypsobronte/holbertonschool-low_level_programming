6a77975c39d88929b9190514bcc5095482b111d9
Lina María Montaño Ramírez - calypsobronte <codelinamaria@gmail.com>
2019-10-15 14:56:02 +0000

    0. Float like a butterfly, sting like a bee

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,2 +1,21 @@
+#include "holberton.h"
 #include <stdio.h> 
 #include <stdlib.h>
+char *create_array(unsigned int size, char c)
+{
+char *p;
+if (size == 0)
+{
+return (NULL);
+}
+else
+{
+p = malloc(sizeof(char) * size);
+while (size--)
+{
+p[size] = c;
+}
+return (p);
+}
+return (NULL);
+}
