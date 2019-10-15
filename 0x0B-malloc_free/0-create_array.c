66fc3a2d9414ef18c8e651ad158056808f58464b
Lina María Montaño Ramírez - calypsobronte <codelinamaria@gmail.com>
2019-10-15 15:01:14 +0000

    Description in the function requrient betty

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,6 +1,12 @@
 #include "holberton.h"
 #include <stdio.h> 
 #include <stdlib.h>
+/**
+ * create_array - pasar el tama;o dependiendo de la cadena
+ * @size: intero
+ * @c: cadena
+ * Return: NULL
+ */
 char *create_array(unsigned int size, char c)
 {
 char *p;
