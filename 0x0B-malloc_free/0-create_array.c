7e7179b30f8a8880d51a76c402afaee06fd1df36
Lina María Montaño Ramírez - calypsobronte <codelinamaria@gmail.com>
2019-10-15 18:32:14 +0000

    Fix task 0-create_array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,15 +11,15 @@ char *create_array(unsigned int size, char c)
 {
 char *p;
 unsigned int a = 0;
-if (size == 0)
+if (size != 0)
+{
+p = malloc(size * sizeof(c));
+}
+else
 {
 return (NULL);
 }
-if (size != 0)
-{
-p = malloc(sizeof(c) * size);
-}
-else
+if (size == 0)
 {
 return (NULL);
 }
