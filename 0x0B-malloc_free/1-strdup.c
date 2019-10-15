5c94638692cdeb9521384c77c02e794551c1c7e0
Lina María Montaño Ramírez - calypsobronte <codelinamaria@gmail.com>
2019-10-15 18:52:44 +0000

    Fix task 1-strdup

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -9,7 +9,7 @@
 char *_strdup(char *str)
 {
 char *p;
-unsigned int a = 0 , i = 0;
+unsigned int a = 0, i = 0;
 if (str == 0)
 return (NULL);
 else
@@ -18,6 +18,10 @@ while (str[a] != 0)
 a++;
 }
 p = malloc(a * sizeof(char));
+if (p == 0)
+{
+return (NULL);
+}
 while (i < a)
 {
 p[i] = *str;
