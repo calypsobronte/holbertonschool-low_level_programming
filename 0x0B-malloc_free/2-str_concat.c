37d2b6b541c27df0b7382b7829773aa8955544d4
Lina María Montaño Ramírez - calypsobronte <codelinamaria@gmail.com>
2019-10-15 16:24:37 +0000

    2. He who is not courageous enough to take risks will accomplish nothing in life

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,2 +1,38 @@
+#include "holberton.h"
 #include <stdio.h> 
 #include <stdlib.h>
+char *str_concat(char *s1, char *s2)
+{
+unsigned int i = 0, j = 0, k = 0, l = 0;
+char *p;
+if (s1 == NULL || s2 == NULL)
+return ("");
+else
+{
+while (s1[i] != 0)
+{
+i++;
+}
+while (s2[j] != 0)
+{
+j++;
+}
+k = i + j;
+p = malloc(k * sizeof(char));
+while (l < i)
+{
+p[l] = *s1;
+s1++;
+l++;
+}
+l = 0;
+while (l < j)
+{
+p[i + l] = *s2;
+s2++;
+l++;
+}
+return (p);
+}
+return (NULL);
+}
