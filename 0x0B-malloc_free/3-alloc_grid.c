42e4380add796f582978b6870bbc980ad192ecd2
Lina María Montaño Ramírez - calypsobronte <codelinamaria@gmail.com>
2019-10-16 03:14:01 +0000

    Complete task 3-alloc_grind 3. If you even dream of beating me you'd better wake up and apologize

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,22 +1,28 @@
 #include "holberton.h"
-#include <stdio.h> 
+#include <stdio.h>
 #include <stdlib.h>
+/**
+ * alloc_grid - pointer a pointer
+ * @width: interp
+ * @height: intero
+ * Return: arr
+*/
 int **alloc_grid(int width, int height)
 {
 if (width <= 0 || height <= 0)
 return (NULL);
 else
 {
-char **arr = (int **) malloc(height * (sizeof(int *)));
-int a, b, c;
+int **arr =  malloc(height * (sizeof(int *)));
+int a, b;
 if (arr == NULL)
 {
 free(arr);
-return (NULL)
+return (NULL);
 }
 for (a = 0; a < height; a++)
 {
-arr[a] = malloc(width(sizeof(int)));
+arr[a] = malloc(width * (sizeof(int)));
 if (arr[a] == NULL)
 {
 for (b = a; b >= 0; b++)
@@ -27,6 +33,13 @@ free(arr);
 return (NULL);
 }
 }
+for (a = 0; a < height; a++)
+{
+for (b = 0; b < width; b++)
+{
+arr[a][b] = 0;
 }
-
+}
+return (arr);
+}
 }
