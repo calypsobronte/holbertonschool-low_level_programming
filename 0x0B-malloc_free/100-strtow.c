4273403b7180172f4500985e0983fe493d71eca5
Lina María Montaño Ramírez - calypsobronte <codelinamaria@gmail.com>
2019-10-16 05:23:37 +0000

    Incomplete task challenge 100

--- 0x0B-malloc_free/100-strtow.c
+++ 0x0B-malloc_free/100-strtow.c
@@ -1,2 +1,17 @@
-#include <stdio.h> 
+#include "holberton.h"
+#include <stdio.h>
 #include <stdlib.h>
+/**
+ * strtow - h
+ * @str: point en string
+ * Return: NULL
+*/
+char **strtow(char *str)
+{
+char **a;
+int i, k;
+if (str == NULL)
+return (NULL);
+if (*str == '\0')
+return (NULL);
+}
