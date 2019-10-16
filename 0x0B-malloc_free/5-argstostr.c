4b74664515565c2cbf02b44bc3e7da947aec4e98
Lina María Montaño Ramírez - calypsobronte <codelinamaria@gmail.com>
2019-10-16 03:53:38 +0000

    5. It isn't the mountains ahead to climb that wear you out; it's the pebble in your shoe

--- 0x0B-malloc_free/5-argstostr.c
+++ 0x0B-malloc_free/5-argstostr.c
@@ -1,2 +1,37 @@
-#include <stdio.h> 
+#include "holberton.h"
+#include <stdio.h>
 #include <stdlib.h>
+/**
+ * argstostr - punt
+ * @ac: intero
+ * @av: char
+ * Returm: p
+*/
+char *argstostr(int ac, char **av)
+{
+int a, b, c, d, e;
+char *p;
+if (ac == 0)
+return (NULL);
+if (av == 0)
+return (NULL);
+a = b = c = d = 0;
+for (a = 0; av[a]; a++)
+{
+for (b = 0; av[a][b]; b++)
+c++;
+}
+e = ac + 1;
+p = (char *)malloc(c * sizeof(char) + e);
+if (p == NULL)
+return (NULL);
+for (a = 0; av[a]; a++)
+{
+for (b = 0; av[a][b]; b++, d++)
+p[d] = av[a][b];
+p[d] = '\n';
+d++;
+}
+p[d] = '\0';
+return (p);
+}
