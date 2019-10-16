e9616ebed8cfe5ba24f283d931de7140d037222e
Lina María Montaño Ramírez - calypsobronte <codelinamaria@gmail.com>
2019-10-16 03:26:46 +0000

    Touch to make point three first to make this one work.4. It's not bragging if you can back it up

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,2 +1,10 @@
-#include <stdio.h> 
+#include "holberton.h"
+#include <stdio.h>
 #include <stdlib.h>
+void free_grid(int **grid, int height)
+{
+int a;
+for (a = 0; a < height; a++)
+free(grid[a]);
+free(grid);
+}
