49bb91af2af09eb6aac8d9491a80c90c907c4b9d
Lina María Montaño Ramírez - calypsobronte <codelinamaria@gmail.com>
2019-10-16 00:47:47 +0000

    Incomplete task 3-alloc_grind

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -3,5 +3,30 @@
 #include <stdlib.h>
 int **alloc_grid(int width, int height)
 {
-char *arr = 
+if (width <= 0 || height <= 0)
+return (NULL);
+else
+{
+char **arr = (int **) malloc(height * (sizeof(int *)));
+int a, b, c;
+if (arr == NULL)
+{
+free(arr);
+return (NULL)
+}
+for (a = 0; a < height; a++)
+{
+arr[a] = malloc(width(sizeof(int)));
+if (arr[a] == NULL)
+{
+for (b = a; b >= 0; b++)
+{
+free(arr[a]);
+}
+free(arr);
+return (NULL);
+}
+}
+}
+
 }
