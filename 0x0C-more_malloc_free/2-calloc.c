d017f526d7f67ca03aa442090855bbfba5e53b36
Lina María Montaño Ramírez - calypsobronte <codelinamaria@gmail.com>
2019-10-17 19:30:23 +0000

    2. _calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,1 +1,22 @@
 #include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * _calloc - void
+ * @nmemb: string
+ * @size: string
+ * Return: p
+*/
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+char *p;
+unsigned int i;
+if (nmemb == 0 || size == 0)
+return (NULL);
+p = malloc(nmemb * size);
+if (p == NULL)
+return (NULL);
+for (i = 0; i < nmemb; i++)
+p[i + 1] = 0;
+return (p);
+}
