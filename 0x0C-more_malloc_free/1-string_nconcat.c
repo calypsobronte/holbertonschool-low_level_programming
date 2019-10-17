30604a80ddfabc59eacc4d22b53a6b0e297198cb
Lina María Montaño Ramírez - calypsobronte <codelinamaria@gmail.com>
2019-10-17 18:09:29 +0000

    1. string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,1 +1,42 @@
 #include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * string_nconcat - void
+ * @s1: string
+ * @s2: string
+ * @n: unsigned integer
+ * Return: p
+*/
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+unsigned int j;
+char *p;
+int i = 0;
+if (s1 == NULL)
+s1 = "";
+if (s2 == NULL)
+s2 = "";
+while (s1[i] != 0)
+{
+i++;
+}
+p = malloc((i + n + 1) * (sizeof(char)));
+if (p == NULL)
+{
+return (NULL);
+}
+i = j = 0;
+while (s1[i] != 0)
+{
+p[i] = s1[i];
+i++;
+}
+while (j < n && s2[j] != 0)
+{
+p[i] = s2[j];
+i++, j++;
+}
+p[i] = '\0';
+return (p);
+}
--- 0x0C-more_malloc_free/holberton.h
+++ 0x0C-more_malloc_free/holberton.h
@@ -1,2 +1,3 @@
 /* Libraries Holberton School */
 void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
