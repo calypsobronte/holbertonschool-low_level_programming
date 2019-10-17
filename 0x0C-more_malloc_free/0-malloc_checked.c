d5479fd8d01fb5bd08731582e67b2255a6ee8e52
Lina María Montaño Ramírez - calypsobronte <codelinamaria@gmail.com>
2019-10-17 15:45:30 +0000

    0x0C. C - More malloc, free

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * malloc_checked - void
+ * @b: integer
+ * Return: *p
+*/
+void *malloc_checked(unsigned int b)
+{
+void *p;
+p = malloc(b);
+if (p == NULL)
+exit(98);
+return (p);
+}
---
+++ 0x0C-more_malloc_free/holberton.h
@@ -0,0 +1,2 @@
+/* Libraries Holberton School */
+void *malloc_checked(unsigned int b);
