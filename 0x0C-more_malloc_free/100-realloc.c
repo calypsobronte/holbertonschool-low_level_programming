6fc0d89a19bacedf5026f80c5d7bf59b1b006c9d
Lina María Montaño Ramírez - calypsobronte <codelinamaria@gmail.com>
2019-10-18 03:37:54 +0000

    Advanced 4. _realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,1 +1,53 @@
 #include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * _realloc - memory in blocks
+ * @ptr: string
+ * @old_size: string
+ * @new_size: string
+ * Return: p
+*/
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+unsigned char *p;
+unsigned int i;
+if (new_size == old_size)
+{
+return (ptr);
+}
+if (new_size == 0 && ptr != NULL)
+{
+free(ptr);
+return (ptr);
+}
+if (ptr == NULL)
+{
+ptr = malloc(new_size * (sizeof(void *)));
+if (ptr == NULL)
+{
+return (NULL);
+}
+return (ptr);
+}
+p = malloc(new_size * (sizeof(char)));
+if (p == NULL)
+{
+return (NULL);
+}
+if (new_size > old_size)
+{
+for (i = 0; i < old_size; i++)
+{
+p[i] = ((char *)ptr)[i];
+}
+free(ptr);
+return (p);
+}
+for (i = 0; i < new_size; i++)
+{
+p[i] = ((char *)ptr)[i];
+}
+free(ptr);
+return (p);
+}
