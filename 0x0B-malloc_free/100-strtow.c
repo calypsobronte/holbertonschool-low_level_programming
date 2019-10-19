0f380b55df3d83d1e96acfb2ab74a7e09fe9154f
Lina María Montaño Ramírez - calypsobronte <codelinamaria@gmail.com>
2019-10-19 12:09:51 +0000

    6. I will show you how great I am

--- 0x0B-malloc_free/100-strtow.c
+++ 0x0B-malloc_free/100-strtow.c
@@ -1,17 +1,51 @@
-#include "holberton.h"
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
+
 /**
- * strtow - h
- * @str: point en string
- * Return: NULL
+* **strtow - splits a string into words
+*
+* @str: string to split
+* Return: pointer to array of strings, or NULL if fail
 */
 char **strtow(char *str)
 {
 char **a;
-int i, k;
-if (str == NULL)
+int i, k, n, cont = 0, len = 0, actual = 0;
+
+if (str == NULL || *str == '\0')
 return (NULL);
-if (*str == '\0')
+for (i = 0; *(str + i) != '\0'; i++)
+{
+if (*(str + i) != ' ')
+cont++;
+while (*(str + i) != ' ' && *(str + i))
+i++;
+if (!*(str + i))
+break;
+}
+if (cont == 0)
 return (NULL);
+a = malloc(sizeof(char *) * (cont + 1));
+if (a == NULL)
+return (NULL);
+for (i = 0, k = i; actual < cont; i++, len = 0, k = i)
+{
+if (*(str + i) == ' ')
+continue;
+while (*(str + k) != ' ' && *(str + k++))
+len++;
+a[actual] = malloc(sizeof(char) * len + 1);
+if (!a[actual])
+{
+while (actual-- >= 0)
+free(a[actual]);
+free(a);
+return (NULL);
+}
+for (n = 0; i < k; i++, n++)
+a[actual][n] = *(str + i);
+a[actual++][n] = '\0';
+}
+a[cont] = NULL;
+return (a);
 }
