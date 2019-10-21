fe7d7b65fdf6ea9fb17b21b56da2e34ab18080d4
Lina María Montaño Ramírez - calypsobronte <codelinamaria@gmail.com>
2019-10-21 17:41:27 +0000

    Fix task all the file

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+#include <stdio.h>
 /**
  * init_dog - copy of var
  * @name: char
@@ -14,6 +15,6 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
     {
     (*d).name = name;
     (*d).age = age;
-    *d->owner = owner;
+    (*d).owner = owner;
     }
 }
