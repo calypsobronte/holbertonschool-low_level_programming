2fd45710cb6de446017c07ce62f1ee17c76c41a0
Lina María Montaño Ramírez - calypsobronte <codelinamaria@gmail.com>
2019-10-21 17:40:26 +0000

    Fix task all the file

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+/**
+ * init_dog - copy of var
+ * @name: char
+ * @age: float
+ * @owner: char
+ * @d: struct
+ * @dog: struct
+ * Return: point p
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+    if (d != NULL)
+    {
+    (*d).name = name;
+    (*d).age = age;
+    *d->owner = owner;
+    }
+}
