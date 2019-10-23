4104a83f171af6e1e4d646fd325bba27e6e9e1eb
Lina María Montaño Ramírez - calypsobronte <codelinamaria@gmail.com>
2019-10-23 16:13:58 +0000

    Create file 2. To hell with circumstances; I create opportunities

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,1 +1,21 @@
-#include function_pointers.h
+#include "function_pointers.h"
+#include <stdio.h>
+/**
+ * array_iterator - parametro en cada elemento
+ * @array: parameter
+ * @size: size_t
+ * @cmp: pointer
+ * Return: 0
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+int a;
+if (array == NULL || cmp == NULL || size <= 0)
+return (-1);
+for (a = 0; a < size; a++)
+if (cmp(array[a]) != 0)
+{
+return(a);
+}
+return (-1);
+}
