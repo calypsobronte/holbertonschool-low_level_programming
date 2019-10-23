5c91ed26bab486cbbd7d7559cebbf41345535101
Lina María Montaño Ramírez - calypsobronte <codelinamaria@gmail.com>
2019-10-23 16:01:48 +0000

    Create file 1. If you spend too much time thinking about a thing, you'll never get it done

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,1 +1,19 @@
-#include function_pointers.h
+#include "function_pointers.h"
+#include <stdio.h>
+/**
+ * array_iterator - parametro en cada elemento
+ * @array: parameter
+ * @size: size_t
+ * @action: pointer
+ * Return: 0
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+unsigned int a;
+if (array == NULL || action == NULL)
+return;
+for (a = 0; a < size; a++)
+{
+action(array[a]);
+}
+}
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,5 +1,7 @@
 /* libraries Holberton */
 #ifndef FunctionHolberton
 #define FunctionHolberton
+#include <stdio.h>
 void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
 #endif /* function holberton */
