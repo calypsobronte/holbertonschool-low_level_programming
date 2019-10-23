3e909ee0abd508943f2e196249bc9482cef36db6
Lina María Montaño Ramírez - calypsobronte <codelinamaria@gmail.com>
2019-10-23 15:27:24 +0000

    Create file 0-print_name.c 0. What's my name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,1 +1,14 @@
-#include function_pointers.h
+#include "function_pointers.h"
+#include <stdio.h>
+/**
+ * print_name - funcion que me permite imprimir el nombre que me trae el main
+ * @name: parameter
+ * @f: pointer
+ * Return: 0
+ */
+void print_name(char *name, void (*f)(char *))
+{
+if (name == NULL || f == NULL)
+return;
+f(name);
+}
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,1 +1,5 @@
 /* libraries Holberton */
+#ifndef FunctionHolberton
+#define FunctionHolberton
+void print_name(char *name, void (*f)(char *));
+#endif /* function holberton */
