eb453fff76a9ead44716d90ba1a09bf8a8e01a02
Lina María Montaño Ramírez - calypsobronte <codelinamaria@gmail.com>
2019-10-25 15:20:16 +0000

    Created file of 0-sum_them_all 0. Beauty is variable, ugliness is constant

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,1 +1,29 @@
 #include "variadic_functions.h"
+#include <stdarg.h>
+/**
+ * sum_them_all - function that returns all sums of a parameter
+ * @n: var unsigned integer
+ * Return: sum
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+va_list valist;
+unsigned int sum = 0;
+unsigned int a;
+/* initialize valist for num number of arguments */
+va_start(valist, n);
+/* if an empty parameter is sent return 0 */
+if (n == 0)
+{
+return (0);
+}
+/* access all the arguments assigned to valist */
+for (a = 0; a < n; a++)
+{
+sum += va_arg(valist, unsigned int);
+}
+/* clean memory reserved for valist */
+va_end(valist);
+/* return sum of the numbers */
+return (sum);
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,1 +1,4 @@
-/* libraries Holberton */
+#ifndef variable_arguments
+#define variable_arguments
+int sum_them_all(const unsigned int n, ...);
+#endif /* libraries Holberton */
