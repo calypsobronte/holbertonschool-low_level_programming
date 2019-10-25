5056ccd0cd103517cd4333ffbbe1edd80954369f
Lina María Montaño Ramírez - calypsobronte <codelinamaria@gmail.com>
2019-10-25 16:18:44 +0000

    Created file 2-print_string 2. One woman's constant is another woman's variable

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,1 +1,32 @@
 #include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+/**
+ * print_strings - function that print strings
+ * @separator: var const char point
+ * @n: var const unsigned int
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+va_list valist;
+unsigned int a;
+/* initialize valist for num number of arguments */
+va_start(valist, n);
+/* if an empty parameter is sent return 0 */
+if (n == 0)
+{
+return;
+}
+/* access all the arguments assigned to valist */
+for (a = 0; a < n; a++)
+{
+printf("%s", va_arg(valist, char *));
+if (a != (n - 1) && separator != NULL)
+{
+printf("%s", separator);
+}
+}
+printf("\n");
+/* clean memory reserved for valist */
+va_end(valist);
+}
