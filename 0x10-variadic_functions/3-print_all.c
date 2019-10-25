fdd2b5f0e960bb89158e171b3d913419843ff29a
Lina María Montaño Ramírez - calypsobronte <codelinamaria@gmail.com>
2019-10-25 18:50:15 +0000

    Created 3-print_all

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,1 +1,46 @@
 #include "variadic_functions.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdarg.h>
+/**
+ * print_all - function that print strings
+ * @format: list of arguments c, i, f, *s
+ */
+void print_all(const char * const format, ...)
+{
+unsigned int a = 0, b = 0;
+char *string;
+va_list valist;
+va_start(valist, format);
+while (format && format[a])
+{
+    switch (format[a])
+    {
+    case 'c': printf("%c, ", va_arg(valist, int));
+        break;
+    case 'i': printf("%i, ", va_arg(valist, int));
+        break;
+    case 'f': printf("%f, ", va_arg(valist, double));
+        break;
+    case 's':
+    string = va_arg(valist, char *);
+    if (string == NULL)
+    {
+    string = "(nil)";
+    }
+    printf("%s", string);
+        break;
+    default:
+        break;
+    }
+    b = a + 1;
+    while (format[b] && (format[b] == 'c' || format[b] == 'i'|| format[b] == 'f' || format[b] == 's'))
+    {
+        break;
+    }
+    a++;
+}
+printf("\n");
+va_end(valist);
+return;
+}
