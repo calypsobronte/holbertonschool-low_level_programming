c83537f65f11c959ce0d72b9693ce92342f8a827
Lina María Montaño Ramírez - calypsobronte <codelinamaria@gmail.com>
2019-10-26 23:17:43 +0000

    Fix task Assembly

--- 0x10-variadic_functions/100-hello_holberton.asm
+++ 0x10-variadic_functions/100-hello_holberton.asm
@@ -1,35 +1,17 @@
-global _start
-
-;
-; CONSTANTS
-;
-SYS_WRITE   equ 1
-SYS_EXIT    equ 60
-STDOUT      equ 1
-
-;
-; Initialised data goes here
-;
 SECTION .data
-hello           db  "Hello, holberton", 10      ; char *
-hello_len       equ $-hello                 ; size_t
+hello: db  "Hello, holberton", 10.0
 
-;
-; Code goes here
-;
 SECTION .text
-
-_start:
-    ; syscall(SYS_WRITE, STDOUT, hello, hello_len);
-    mov     rax, SYS_WRITE
-    mov     rdi, STDOUT
+global main
+main:
+    mov     rax, 1
+    mov     rdi, 1
     mov     rsi, hello
-    mov     rdx, hello_len
+    mov     rdx, 17
     syscall
-    push    rax
+    ;return (0)
 
     ; syscall(SYS_EXIT, <sys_write return value> - hello_len);
-    mov     rax, SYS_EXIT
-    pop     rdi
-    sub     rdi, hello_len
+    mov     rax, 60
+    mov     rdi, 1
     syscall
