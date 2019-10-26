9c9328364bc5f7482c3766e7b594617310d20211
Lina María Montaño Ramírez - calypsobronte <codelinamaria@gmail.com>
2019-10-26 18:37:52 +0000

    Fix task 3 trailing statements should be on next line, line over 80 characters, space, others

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -14,31 +14,35 @@ va_list valist;
 va_start(valist, format);
 while (format && format[a])
 {
-    switch (format[a])
-    {
-    case 'c': printf("%c, ", va_arg(valist, int));
-        break;
-    case 'i': printf("%i, ", va_arg(valist, int));
-        break;
-    case 'f': printf("%f, ", va_arg(valist, double));
-        break;
-    case 's':
-    string = va_arg(valist, char *);
-    if (string == NULL)
-    {
-    string = "(nil)";
-    }
-    printf("%s", string);
-        break;
-    default:
-        break;
-    }
-    b = a + 1;
-    while (format[b] && (format[b] == 'c' || format[b] == 'i'|| format[b] == 'f' || format[b] == 's'))
-    {
-        break;
-    }
-    a++;
+switch (format[a])
+{
+case 'c':
+printf("%c, ", va_arg(valist, int));
+break;
+case 'i':
+printf("%i, ", va_arg(valist, int));
+break;
+case 'f':
+printf("%f, ", va_arg(valist, double));
+break;
+case 's':
+string = va_arg(valist, char *);
+if (string == NULL)
+{
+string = "(nil)";
+}
+printf("%s", string);
+break;
+default:
+break;
+}
+b = a + 1;
+while (format[b] && (format[b] == 'c' || format[b] == 'i'
+|| format[b] == 'f' || format[b] == 's'))
+{
+break;
+}
+a++;
 }
 printf("\n");
 va_end(valist);
