f726074e3dd91ca2e511e41ab5ef2ca66e444629
Lina María Montaño Ramírez - calypsobronte <codelinamaria@gmail.com>
2019-10-26 18:12:54 +0000

    Fix task 1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -13,15 +13,10 @@ va_list valist;
 unsigned int a;
 /* initialize valist for num number of arguments */
 va_start(valist, n);
-/* if an empty parameter is sent return 0 */
-if (n == 0)
-{
-return;
-}
 /* access all the arguments assigned to valist */
 for (a = 0; a < n; a++)
 {
-printf("%d", va_arg(valist, int));
+printf("%i", va_arg(valist, int));
 if (a != (n - 1) && separator != NULL)
 {
 printf("%s", separator);
