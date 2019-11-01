158a4f2b5141861a904d02200aad92f907a1ec57
Lina María Montaño Ramírez - calypsobronte <codelinamaria@gmail.com>
2019-11-01 03:10:03 +0000

    Fix task 0 print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -3,7 +3,6 @@
  * print_list - function que print a linked list
  * @h: pointer
  * Return: retur of number of positions
- * 
  */
 size_t print_list(const list_t *h)
 {
@@ -13,9 +12,13 @@ if (h)
 while (h != NULL)
 {
 if (h[0].str != NULL)
-printf("[%d] %s\n", h[0].len, h[0].str);
+{
+printf("[%i] %s\n", h[0].len, h[0].str);
+}
 else
+{
 printf("[0] (nil)\n");
+}
 cont++;
 h = h[0].next;
 }
