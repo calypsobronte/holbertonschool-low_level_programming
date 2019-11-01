51816bd2dbd2d51cbe9516f554fe3370007c1786
Lina María Montaño Ramírez - calypsobronte <codelinamaria@gmail.com>
2019-11-01 04:31:14 +0000

    Advacend Assembly

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,1 +1,16 @@
-#include "lists.h"
+section .data
+    calypso: db "Hello, Holberton", 10,0
+
+section .text
+    global main
+
+main:
+    mov     rax, 1
+    mov     rdi, 1
+    mov     rsi, calypso
+    mov     rdx, 17
+    syscall
+    ;return (0)
+    mov     rax, 60
+    mov     rdi, 0
+    syscall
