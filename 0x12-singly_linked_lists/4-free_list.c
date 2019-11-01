e00c3d25e266797010ac483bc97951d4ec06edda
Lina María Montaño Ramírez - calypsobronte <codelinamaria@gmail.com>
2019-11-01 04:11:50 +0000

    add function task 4 of free list

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,1 +1,17 @@
 #include "lists.h"
+/**
+ * free_list - function que free a list
+ * @head: pointer of list
+ */
+void free_list(list_t *head)
+{
+list_t *free_lists;
+while (head)
+{
+free_lists = head;
+free(head[0].str);
+free(head);
+head = free_lists[0].next;
+}
+free(head);
+}
