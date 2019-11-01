a2c1903682559a5d05658f5d1e34bab30955ef31
Lina María Montaño Ramírez - calypsobronte <codelinamaria@gmail.com>
2019-11-01 04:45:11 +0000

    Advacend the hare and the tortoise for constructor

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,1 +1,10 @@
-#include "lists.h"
+#include <stdio.h>
+void myStartupFun(void) __attribute__ ((constructor));
+/**
+ * myStartupFun - function print befero main
+*/
+void myStartupFun(void)
+{
+printf("You're beat! and yet, you must allow,\n
+I bore my house upon my back!\n");
+}
