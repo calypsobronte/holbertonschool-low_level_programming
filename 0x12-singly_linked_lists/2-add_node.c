054a4ad9f61b02571a1d20c622d935b8f5ea8ffb
Lina María Montaño Ramírez - calypsobronte <codelinamaria@gmail.com>
2019-11-01 03:49:08 +0000

    created task 2 add node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,1 +1,26 @@
 #include "lists.h"
+/**
+ * add_node - function que print a linked list
+ * @head: pointer of list
+ * @str: point
+ * Return: return of address of new element
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+list_t *new_element;
+int a = 0;
+new_element = malloc(sizeof(list_t));
+if (new_element == NULL)
+{
+return (NULL);
+}
+new_element[0].str = strdup(str);
+new_element[0].next = *head;
+*head = new_element;
+while (str[a])
+{
+a++;
+}
+new_element[0].len = a;
+return (new_element);
+}
