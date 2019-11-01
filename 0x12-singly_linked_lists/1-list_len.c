a6ac5813abaddaa6ba857e19444d6dc3066702cb
Lina María Montaño Ramírez - calypsobronte <codelinamaria@gmail.com>
2019-11-01 03:27:27 +0000

    add function in list len

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,1 +1,16 @@
 #include "lists.h"
+/**
+ * list_len - function que print a linked list
+ * @h: pointer
+ * Return: retur of number of positions
+ */
+size_t list_len(const list_t *h)
+{
+unsigned int cont = 0;
+while (h != NULL)
+{
+h = h[0].next;
+cont++;
+}
+return (cont);
+}
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -20,4 +20,5 @@ unsigned int len;
 struct list_s *next;
 } list_t;
 size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
 #endif /*Libraries Holberton of Lists linked*/
