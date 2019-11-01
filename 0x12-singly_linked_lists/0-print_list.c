23ff7bfbe4c3b97ef38779c7b8fdfcaad90b9b7f
Lina María Montaño Ramírez - calypsobronte <codelinamaria@gmail.com>
2019-11-01 03:04:37 +0000

    my firts list print

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,1 +1,24 @@
 #include "lists.h"
+/**
+ * print_list - function que print a linked list
+ * @h: pointer
+ * Return: retur of number of positions
+ * 
+ */
+size_t print_list(const list_t *h)
+{
+unsigned int cont;
+if (h)
+{
+while (h != NULL)
+{
+if (h[0].str != NULL)
+printf("[%d] %s\n", h[0].len, h[0].str);
+else
+printf("[0] (nil)\n");
+cont++;
+h = h[0].next;
+}
+}
+return (cont);
+}
