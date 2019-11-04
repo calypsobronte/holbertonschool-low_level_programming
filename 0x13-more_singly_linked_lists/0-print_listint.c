ffcab2f36b0fced0249d58c352a5c19327f05303
Lina María Montaño Ramírez - calypsobronte <codelinamaria@gmail.com>
2019-11-04 15:04:32 +0000

    add print linked list integer task 0-print_listint

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,1 +1,17 @@
 #include "lists.h"
+/**
+ * print_listint - function que print a linked list integer
+ * @h: pointer
+ * Return: retur of number of positions
+ */
+size_t print_listint(const listint_t *h)
+{
+unsigned int cont = 0;
+while (h != NULL)
+{
+printf("%d\n", h[0].n);
+h = h[0].next;
+cont++;
+}
+return (cont);
+}
