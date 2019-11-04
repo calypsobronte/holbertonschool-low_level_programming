51fd7b2d92494518bf917d739cb2990c5bdc15e8
Lina María Montaño Ramírez - calypsobronte <codelinamaria@gmail.com>
2019-11-04 15:21:46 +0000

    add print len  task 1-print_len

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,1 +1,16 @@
 #include "lists.h"
+/**
+ * listint_len - function que print a linked list integer
+ * @h: pointer
+ * Return: retur of number of positions
+ */
+size_t listint_len(const listint_t *h)
+{
+unsigned int cont = 0;
+while (h != NULL)
+{
+h = h[0].next;
+cont++;
+}
+return (cont);
+}
