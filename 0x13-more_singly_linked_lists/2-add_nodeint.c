4c872fca20a234cb345b9ec029f0b1a5b84347c8
Lina María Montaño Ramírez - calypsobronte <codelinamaria@gmail.com>
2019-11-04 15:26:32 +0000

    add print add node task 2

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,1 +1,16 @@
 #include "lists.h"
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+listint_t *new_element;
+new_element = malloc(sizeof(listint_t));
+if (new_element == NULL)
+{
+return (NULL);
+}
+
+new_element[0].n = n;
+new_element[0].next = *head;
+*head = new_element;
+
+return (new_element);
+}
