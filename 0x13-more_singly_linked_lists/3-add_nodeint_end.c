8178af79a48c0c4e980994c3be37c819ab6e6b12
Lina María Montaño Ramírez - calypsobronte <codelinamaria@gmail.com>
2019-11-04 15:49:04 +0000

    add task 3-add_node_integer_end

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,1 +1,29 @@
 #include "lists.h"
+/**
+ * *add_nodeint_end - function que print a list add nodes integer with end
+ * @head: pointer of list
+ * @n: const integer
+ * Return: return of address of new element
+ */
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+listint_t *new, *end = *head;
+new = malloc(sizeof(listint_t));
+if (new == NULL)
+{
+return (NULL);
+}
+new[0].n = n;
+new[0].next = NULL;
+if (*head == NULL)
+{
+*head = new;
+return (new);
+}
+while (end[0].next != NULL)
+{
+end = end[0].next;
+}
+end[0].next = new;
+return (new);
+}
