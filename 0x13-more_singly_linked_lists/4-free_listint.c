4ff0b53bc91c5bb4e4b232a6b841efa659250c97
Lina María Montaño Ramírez - calypsobronte <codelinamaria@gmail.com>
2019-11-04 16:05:50 +0000

    add task 4-free_listint

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,1 +1,16 @@
 #include "lists.h"
+/**
+ * free_listint - function que free momery
+ * @head: pointer of list
+ */
+void free_listint(listint_t *head)
+{
+listint_t *free_lists;
+while (head)
+{
+free_lists = head;
+free(head);
+head = free_lists[0].next;
+}
+free(head);
+}
