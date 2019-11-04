5fa80a5bc120513abac290009afea27d9a992e8d
Lina María Montaño Ramírez - calypsobronte <codelinamaria@gmail.com>
2019-11-04 19:24:06 +0000

    add task 7 function that returns the nth node of a listint_t linked list.

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,1 +1,26 @@
 #include "lists.h"
+/**
+ * get_nodeint_at_index - function get node int at index
+ * @head: pointer of list
+ * @index: integer
+ * Return: return of address of new_list
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+unsigned int temp = 0;
+listint_t *new_list, *aux = head;
+while (temp < index)
+{
+if (aux != NULL)
+{
+aux = aux[0].next;
+temp++;
+}
+else
+{
+return (NULL);
+}
+}
+new_list = aux;
+return (new_list);
+}
