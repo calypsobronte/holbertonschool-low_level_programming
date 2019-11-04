bce469eedb718aa1ebc9db43daf224a5f6d05010
Lina María Montaño Ramírez - calypsobronte <codelinamaria@gmail.com>
2019-11-04 15:37:29 +0000

    Fix task for betty in lists, 0, 1, 2

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,4 +1,10 @@
 #include "lists.h"
+/**
+ * *add_nodeint - function que print to the add nodes integer
+ * @head: pointer
+ * @n: integer
+ * Return: retur of number of positions
+ */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
 listint_t *new_element;
@@ -7,10 +13,8 @@ if (new_element == NULL)
 {
 return (NULL);
 }
-
 new_element[0].n = n;
 new_element[0].next = *head;
 *head = new_element;
-
 return (new_element);
 }
