769765b575971463f194565af38d0e17819678fb
Lina María Montaño Ramírez - calypsobronte <codelinamaria@gmail.com>
2019-11-05 22:52:44 +0000

    Add task advanced 11 function that reverses a listint_t linked list.

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -1,1 +1,21 @@
 #include "lists.h"
+/**
+ * delete_nodeint_at_index - delete the node at index
+ * @head: point of point
+ * @index: of node
+ * Return: 0
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+listint_t *actual = NULL, *temp;
+while (head != NULL)
+{
+actual = (*head[0]).next;
+(*head[0]).next = temp;
+temp = *head;
+*head = actual;
+}
+*head = temp;
+return (*head);
+}
+
