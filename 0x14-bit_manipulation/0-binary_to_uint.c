3294e2ec70bdb3241f762042e7faad7ca1fe56b6
Lina María Montaño Ramírez - calypsobronte <codelinamaria@gmail.com>
2019-11-07 19:28:06 +0000

    add task 0 convert binary to unit

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,1 +1,30 @@
 #include "holberton.h"
+/**
+ * binary_to_uint - converts a binary number to an unsigned int
+ * @b: point char
+ * Return: result of sum
+ */
+unsigned int binary_to_uint(const char *b)
+{
+unsigned int sum = 0;
+int cont = 0, next = 0, mult = 1;
+if (b == NULL)
+return (0);
+while (b[cont] != '\0')
+{
+cont++;
+}
+for (next = cont - 1; next >= 0; next--)
+{
+if (b[next] != '1' && b[next] != '0')
+{
+return (0);
+}
+if (b[next] == '1')
+{
+sum += mult;
+}
+mult *= 2;
+}
+return (sum);
+}
--- 0x14-bit_manipulation/holberton.h
+++ 0x14-bit_manipulation/holberton.h
@@ -1,1 +1,8 @@
-/* libraries Holberton */
+#ifndef bit
+#define bit
+#include <stdio.h>
+#include <stdarg.h>
+#include <string.h>
+#include <stdlib.h>
+unsigned int binary_to_uint(const char *b);
+#endif /* libraries Holberton of bit manipulation */
