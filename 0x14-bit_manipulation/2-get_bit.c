aab4e0591a52c92e39163b32c8e3b1fce3f67858
Lina María Montaño Ramírez - calypsobronte <codelinamaria@gmail.com>
2019-11-07 22:14:09 +0000

    add task 2 get bit

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,1 +1,26 @@
 #include "holberton.h"
+/**
+ * 
+ * 
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+unsigned int cont = 0;
+if (index > (8 * 8))
+{
+    return (-1);
+}
+else
+{
+while (index > cont)
+{
+n /= 2;
+cont++;
+}
+if (index == cont)
+{
+return (n % 2);
+}
+}
+return (-1);
+}
