94f3ffcd5177ede1735f58edc097df4397f42276
Lina María Montaño Ramírez - calypsobronte <codelinamaria@gmail.com>
2019-11-07 22:57:02 +0000

    Requirement task 3 set bit

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,4 +1,10 @@
 #include "holberton.h"
+/**
+ * set_bit - sets the value of a bit to 1 at a given index.
+ * @n: point
+ * @index: int
+ * Return: -1
+ */
 int set_bit(unsigned long int *n, unsigned int index)
 {
 unsigned long int *point;
