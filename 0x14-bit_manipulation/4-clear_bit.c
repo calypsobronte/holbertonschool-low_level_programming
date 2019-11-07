2568fece1aaaa621c62804321c492b19ad30e656
Lina María Montaño Ramírez - calypsobronte <codelinamaria@gmail.com>
2019-11-07 23:36:08 +0000

    add task 4 clear bit

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,1 +1,42 @@
 #include "holberton.h"
+/**
+ * clear_bit - sets the value of a bit to 1 at a given index.
+ * @n: point
+ * @index: int
+ * Return: -1
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+unsigned int mult = 1, cont = 0;
+if (n == NULL)
+{
+return (-1);
+}
+if (index > 64)
+{
+return (-1);
+}
+else
+{
+while (cont != index)
+{
+mult *= 2;
+cont++;
+}
+if (cont == index)
+{
+if (*n >= mult)
+{
+*n -= mult;
+printf("%ld\n", *n);
+}
+else
+{
+*n= 0;
+printf("%ld\n", *n);
+}
+return (cont);
+}
+}
+return (-1);
+}
