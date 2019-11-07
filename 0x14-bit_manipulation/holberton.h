b02ffb4338ca92717c05165fa6020a25aaaa4229
Lina María Montaño Ramírez - calypsobronte <codelinamaria@gmail.com>
2019-11-07 20:40:17 +0000

    add task 1 print_binary

--- 0x14-bit_manipulation/holberton.h
+++ 0x14-bit_manipulation/holberton.h
@@ -5,4 +5,6 @@
 #include <string.h>
 #include <stdlib.h>
 unsigned int binary_to_uint(const char *b);
+void print_binary(unsigned long int n);
+int _putchar(char c);
 #endif /* libraries Holberton of bit manipulation */
