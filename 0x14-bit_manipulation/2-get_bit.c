d7771d3213ba9b19bb5d33fb3dd25bb91061db49
Lina María Montaño Ramírez - calypsobronte <codelinamaria@gmail.com>
2019-11-07 22:18:10 +0000

    Requirement  task 2 get bit

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,14 +1,17 @@
 #include "holberton.h"
 /**
- * 
- * 
+ * get_bit - returns the value of a bit at a given index.
+ * @n: int
+ * @index: int
+ * Return: -1
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
 unsigned int cont = 0;
+
 if (index > (8 * 8))
 {
-    return (-1);
+return (-1);
 }
 else
 {
