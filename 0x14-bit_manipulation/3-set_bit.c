45d327b20c136aa8fda7b8b8ded2031083b7dfcc
Lina María Montaño Ramírez - calypsobronte <codelinamaria@gmail.com>
2019-11-07 22:54:21 +0000

    add task 3 set bit

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,1 +1,25 @@
 #include "holberton.h"
+int set_bit(unsigned long int *n, unsigned int index)
+{
+unsigned long int *point;
+unsigned int mult = 1, cont = 0;
+point = malloc(sizeof(unsigned long int) * 8 - 1);
+if (point == NULL)
+{
+return (-1);
+}
+else
+{
+while (cont != index)
+{
+mult *= 2;
+cont++;
+}
+if (cont == index)
+{
+*n += mult;
+return (1);
+}
+}
+return (-1);
+}
