0a1901f9278e90be4d690fd7867c0a87ea21b3ef
Lina María Montaño Ramírez - calypsobronte <codelinamaria@gmail.com>
2019-11-08 13:04:47 +0000

    add task advanced 100 Write a function that checks the endianness.

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,1 +1,18 @@
 #include "holberton.h"
+/**
+ * get_endianness - Write a function that checks the endianness.
+ * Return: 0
+ */
+int get_endianness(void)
+{
+int get = 1;
+get >>= 1;
+if (get == 0)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
