536114d8ffafd223baa27d8ecf4b319bab2bc4f0
Lina María Montaño Ramírez - calypsobronte <codelinamaria@gmail.com>
2019-11-08 12:53:45 +0000

    add task  returns the number of bits

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,1 +1,15 @@
 #include "holberton.h"
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+unsigned long int flip = n ^ m;
+unsigned int cont = 0;
+while (flip)
+{
+if ((flip & 1) == 1)
+{
+cont++;
+}
+flip >>= 1;
+}
+return (cont);
+}
