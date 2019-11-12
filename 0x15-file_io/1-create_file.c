045a5bf82f9f546649155181a8a2f0a823c37c6d
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2019-11-12 20:11:53 +0000

    Requirement in file 1-create_file of betty

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -7,28 +7,27 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-int list, cont = 0, write_create = 0;
+int list, cont = 0, write_create;
 if (filename == NULL)
 {
 return (-1);
 }
-if (text_content == NULL)
-{
-return (-1);
-}
-list = open(filename, O_WRONLY | O_TRUNC | O_CREAT, S_IRUSR | S_IWUSR);
+list = open(filename, O_RDWR | O_TRUNC | O_CREAT, S_IRUSR | S_IWUSR);
 if (list == -1)
 {
 return (-1);
 }
+if (text_content != NULL)
+{
 while (text_content[cont] != '\0')
 {
 cont++;
 }
 write_create = write(list, text_content, cont);
+}
 if (write_create == -1)
 {
-return (0);
+return (-1);
 }
 close(list);
 return (1);
