dc074c7fb71f11ffdf1f79d8331a41206542a0bc
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2019-11-12 16:54:32 +0000

    add file 00. Tread lightly, she is near  Write a function that reads a text file and prints it to the POSIX standard output.

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,1 +1,29 @@
 #include "holberton.h"
+/**
+ * read_textfile - function reads a text file and prints
+ * it to the POSIX standard output.
+ * @filename: char
+ * @letters: list
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+int open_file;
+ssize_t sizeoflist;
+char buffer[letters + 1];
+if (filename == NULL)
+return (0);
+open_file = open(filename, O_RDONLY, 0600);
+if (open_file == -1)
+{
+    return (0);
+}
+sizeoflist = read(open_file, buffer, letters);
+/* if (sizeoflist == -1)
+{
+    return (0);
+} */
+buffer[letters] = '\0';
+close(open_file);
+printf("%s", buffer);
+return (sizeoflist);
+}
--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -1,1 +1,7 @@
-/* libraries Holberton */
+#ifndef bit
+#define bit
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+ssize_t read_textfile(const char *filename, size_t letters);
+#endif /* libraries Holberton */
