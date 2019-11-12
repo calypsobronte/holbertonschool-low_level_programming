97ce6b778f94066cb2d66c892dd51bfabdf7a8ad
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2019-11-12 20:32:38 +0000

    Add file 2-append_text_to_file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,1 +1,34 @@
 #include "holberton.h"
+/**
+ * append_text_to_file - function that appends text at the end of a file.
+ * @filename: char const
+ * @text_content: char
+ * Return: 1
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+int list, cont = 0, write_create;
+if (filename == NULL)
+{
+return (-1);
+}
+list = open(filename, O_RDWR | O_APPEND, S_IRUSR | S_IWUSR);
+if (list == -1)
+{
+return (-1);
+}
+if (text_content != NULL)
+{
+while (text_content[cont] != '\0')
+{
+cont++;
+}
+write_create = write(list, text_content, cont);
+if (write_create == -1)
+{
+return (-1);
+}
+}
+close(list);
+return (1);
+}
