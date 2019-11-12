fc0380172b1e69d0648e4fe714c58698c58721d2
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2019-11-12 18:20:16 +0000

    Fix task 0-read_textfile.c trying

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -10,11 +10,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 int open_file;
 ssize_t sizeoflist;
-char *buffer = malloc(letters);
-if (buffer == NULL)
-{
-return (0);
-}
+char buffer[letters + 1];
 if (filename == NULL)
 return (0);
 open_file = open(filename, O_RDONLY, 0600);
@@ -23,9 +19,15 @@ if (open_file == -1)
 return (0);
 }
 sizeoflist = read(open_file, buffer, letters);
+if (sizeoflist < 1024)
+{
 buffer[letters] = '\0';
-close(open_file);
-write(STDOUT_FILENO, buffer, letters);
-free(buffer);
+printf("%s", buffer);
+}
+else
+{
+printf("%s", buffer);
+}
+
 return (sizeoflist);
 }
