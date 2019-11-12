119b744bb1213f85131ae9bb32512a350d792ead
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2019-11-12 18:59:05 +0000

    Add file 1-create_file with librarie this is incomplete 1. Under the snow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,1 +1,9 @@
 #include "holberton.h"
+int create_file(const char *filename, char *text_content)
+{
+if (filename == NULL)
+{
+return (-1);
+}
+
+}
--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -5,4 +5,5 @@
 #include <fcntl.h>
 #include <stdlib.h>
 ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
 #endif /* libraries Holberton */
