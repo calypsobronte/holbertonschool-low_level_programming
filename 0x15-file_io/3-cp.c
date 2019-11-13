c97caf48647f6171ea35b37a483bfad208a05c44
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2019-11-13 00:42:47 +0000

    Requirement task file 3 cp file_from file_to of betty

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,4 +1,10 @@
 #include "holberton.h"
+/**
+ * main - Write a program that copies the content of a file to another file.
+ * @argc: int
+ * @argv: char
+ * Return: 0
+ */
 int main(int argc, char *argv[])
 {
 int file_from, file_to, file_read, file_write, close_from, close_to;
@@ -11,29 +17,30 @@ exit(97);
 file_from = open(argv[1], O_RDONLY);
 if (file_from == -1)
 {
-dprintf(STDERR_FILENO, "Error: Can't read from file %s\n",argv[1]);
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 exit(98);
 }
-file_to = open(argv[2], O_WRONLY | O_TRUNC | O_CREAT, S_IRUSR | S_IWUSR | S_IXUSR);
+file_to = open(argv[2], O_WRONLY | O_TRUNC | O_CREAT, S_IRUSR
+| S_IWUSR | S_IXUSR);
 while ((file_read = read(file_from, buffer, SIZEOF)) != 0)
 {
 file_write = write(file_to, buffer, file_read);
 if (file_write  == -1)
 {
-dprintf(STDERR_FILENO, "Error: Can't write to %s\n",argv[2]);
+dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 exit(99);
 }
 }
 close_from = close(file_from);
 if (close_from == -1)
 {
-dprintf(STDERR_FILENO, "Error: Can't close to %i\n",close_from);
+dprintf(STDERR_FILENO, "Error: Can't close to %i\n", close_from);
 exit(100);
 }
 close_to = close(file_to);
 if (close_to == -1)
 {
-dprintf(STDERR_FILENO, "Error: Can't close to %i\n",close_to);
+dprintf(STDERR_FILENO, "Error: Can't close to %i\n", close_to);
 exit(100);
 }
 return (0);
