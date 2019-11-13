1351812f97c034ce7e698d56de9fad5a315b9432
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2019-11-13 23:37:28 +0000

    Fix task file 3 cp :( no

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -7,24 +7,21 @@
  */
 int main(int argc, char *argv[])
 {
-int file_from, file_to, file_read, file_write, close_from, close_to;
+int file_from, file_to, file_read, file_write;
 char buffer[SIZEOF];
 if (argc != 3)
 {
-dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-exit(97);
+dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"); exit(97);
 }
 file_from = open(argv[1], O_RDONLY);
 if (file_from == -1)
 {
-dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-exit(98);
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]); exit(98);
 }
-file_to = open(argv[2], O_RDWR | O_CREAT | O_TRUNC, 0664);
+file_to = open(argv[2],  O_CREAT | O_WRONLY | O_TRUNC, 0664);
 if (file_to == -1)
 {
-dprintf(STDERR_FILENO, "Error: Can't write from file %s\n", argv[2]);
-exit(99);
+dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]); exit(99);
 }
 file_read = 1;
 while (file_read)
@@ -32,30 +29,24 @@ while (file_read)
 file_read = read(file_from, buffer, SIZEOF);
 if (file_read == -1)
 {
-dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-exit(98);
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]); exit(98);
 }
 if (file_read > 0)
 {
 file_write = write(file_to, buffer, file_read);
 if (file_write != file_read || file_write  == -1)
 {
-dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-exit(99);
+dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]); exit(99);
 }
 }
 }
-close_from = close(file_from);
-if (close_from == -1)
+if (close(file_from) == -1)
 {
-dprintf(STDERR_FILENO, "Error: Can't close to %i\n", file_from);
-exit(100);
+dprintf(STDERR_FILENO, "Error: Can't close fd  %i\n", file_from); exit(100);
 }
-close_to = close(file_to);
-if (close_to == -1)
+if (close(file_to) == -1)
 {
-dprintf(STDERR_FILENO, "Error: Can't close to %i\n", file_to);
-exit(100);
+dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", file_to); exit(100);
 }
 return (0);
 }
