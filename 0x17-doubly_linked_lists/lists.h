4263f72ce478e43a9d9d4ad4d4eaf53f101c2663
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2019-12-13 21:25:09 +0000

    udpate file task librarie with struct dlistinta_s

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -1,1 +1,23 @@
-/*libraries Doubly linked lists*/
+#ifndef LISTS_DOUBLY
+#define LISTS_DOUBLY
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * struct dlistint_s - doubly linked list
+ * @n: integer
+ * @prev: points to the previous node
+ * @next: points to the next node
+ *
+ * Description: doubly linked list node structure
+ * for Holberton project
+ */
+typedef struct dlistint_s
+{
+int n;
+struct dlistint_s *prev;
+struct dlistint_s *next;
+} dlistint_t;
+
+#endif /*libraries Doubly linked lists*/
