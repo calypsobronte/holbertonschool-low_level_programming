9ae915b903870be9a208de0beb190edb2101a19a
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2019-12-15 20:38:03 +0000

    Fix task for description of function of 8

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,4 +1,11 @@
 #include "lists.h"
+/**
+ * delete_dnodeint_at_index - deletes the node at index index of
+ * a dlistint_t linked list.
+ * @head: **pointer to list
+ * @index: index to insert new_node node
+ * Return: 1
+ */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
 dlistint_t *temp = *head;
