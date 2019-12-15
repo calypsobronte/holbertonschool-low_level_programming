8c6e42128276aeac3526af3ec8f04333aa5dbd8e
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2019-12-15 18:20:25 +0000

    add funtion en lists.h and add function 6. function that returns the sum of all the data

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,1 +1,17 @@
 #include "lists.h"
+/**
+ * sum_dlistint - function that returns the sum of all the data
+ * (n) of a dlistint_t linked list.
+ * @head: list for add
+ * Return: result of add
+ */
+int sum_dlistint(dlistint_t *head)
+{
+int add = 0;
+while (head != NULL)
+{
+add += head->n;
+head = head[0].next;
+}
+return (add);
+}
