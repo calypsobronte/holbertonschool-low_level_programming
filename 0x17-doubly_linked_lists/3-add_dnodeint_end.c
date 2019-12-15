a80a119a089b62a7ca2b7affde7076921ef5af47
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2019-12-15 00:02:27 +0000

    add funtion en lists.h and add function 3. Add node at the end

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,1 +1,25 @@
 #include "lists.h"
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+dlistint_t *new_node, *temp = *head;
+new_node = malloc(sizeof(dlistint_t));
+if (new_node == NULL)
+{
+return (NULL);
+}
+new_node->n = n;
+new_node->next = NULL;
+if (*head != NULL)
+{
+new_node->prev = NULL;
+*head = new_node;
+return (new_node);
+}
+while (temp->next != NULL)
+{
+    temp = temp->next;
+}
+new_node->prev = temp;
+temp->next = new_node;
+return (new_node);
+}
