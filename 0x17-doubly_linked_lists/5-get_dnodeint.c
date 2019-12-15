720297810cd53cd78608eae7df620dee69507daa
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2019-12-15 00:57:32 +0000

    add task 55. Get node at index

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -1,1 +1,21 @@
 #include "lists.h"
+/**
+ * *get_dnodeint_at_index - fucntion than get the node at index
+ * @head: point of list
+ * @index: number of index list
+ * Return: returns null in case it fails
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+unsigned int cont = 0;
+while (head)
+{
+if (cont == index)
+{
+return (head);
+}
+head = head[0].next;
+cont++;
+}
+return (NULL);
+}
