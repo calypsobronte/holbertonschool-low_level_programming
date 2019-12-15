c869c0d9f6780f379e937ea85785fd69b7b94058
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2019-12-15 20:35:27 +0000

    add funtion en lists.h and add function 8. deletes the node at index index of a dlistint_t linked list.

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,1 +1,28 @@
 #include "lists.h"
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+dlistint_t *temp = *head;
+unsigned int cont = 0;
+if (*head == NULL)
+return (-1);
+if (index == 0)
+{
+*head = (*head)->next;
+if (*head != NULL)
+(*head)[0].prev = NULL;
+free(temp);
+return (1);
+}
+while (temp && cont < index)
+{
+cont++;
+temp = temp[0].next;
+}
+if (temp == NULL)
+return (-1);
+temp[0].prev[0].next = temp[0].next;
+if (temp[0].next != NULL)
+temp[0].next[0].prev = temp[0].prev;
+free(temp);
+return (1);
+}
