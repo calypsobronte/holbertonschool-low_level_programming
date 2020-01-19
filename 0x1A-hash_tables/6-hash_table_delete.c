85874df40e412fabe652594933abe5ea215da9e2
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2020-01-19 03:58:06 +0000

    add file task 6. >>> del ht  that deletes a hash table.

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,1 +1,33 @@
 #include "hash_tables.h"
+/**
+ * hash_table_delete - that deletes a hash table.
+ * @ht: s the hash table
+*/
+void hash_table_delete(hash_table_t *ht)
+{
+unsigned int i = 0;
+hash_node_t *tmp;
+hash_node_t *node_t;
+
+if (ht)
+{
+while (i < ht[0].size)
+{
+if (ht[0].array[i] != NULL)
+{
+tmp = ht[0].array[i];
+while (tmp)
+{
+node_t = tmp;
+tmp = tmp[0].next;
+free(node_t[0].key);
+free(node_t[0].value);
+free(node_t);
+}
+}
+i++;
+}
+free(ht[0].array);
+free(ht);
+}
+}
