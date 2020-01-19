96eb499ac3aacb578701799278916e7e1c7c7273
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2020-01-19 03:47:39 +0000

    add file task 4. >>> ht['betty'] retrieves a value associated with a key.

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,1 +1,24 @@
 #include "hash_tables.h"
+/**
+ * hash_table_get - that retrieves a value associated with a key.
+ * @ht: pointer
+ * @key: arrya of key
+ * Return: Null
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+int index;
+hash_node_t *tmp;
+
+if (!ht)
+return (NULL);
+index = key_index((unsigned char *)key, ht[0].size);
+tmp = ht[0].array[index];
+while (tmp)
+{
+if (strcmp(key, tmp[0].key) == 0)
+return (tmp[0].value);
+tmp = tmp[0].next;
+}
+return (NULL);
+}
