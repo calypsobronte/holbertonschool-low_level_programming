3a86cc0660e2e6e22eb11da29a139bc353e46e5b
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2020-01-19 03:53:46 +0000

    add file task 5. >>> print(ht) that prints a hash table.

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -1,1 +1,32 @@
 #include "hash_tables.h"
+void hash_table_print(const hash_table_t *ht)
+{
+unsigned int i = 0;
+int band = 0;
+hash_node_t *tmp;
+
+if (ht != NULL)
+{
+printf("{");
+while (i < ht->size)
+{
+tmp = ht->array[i];
+while (tmp != NULL)
+{
+if (i > 0 && band != 0)
+printf(", ");
+printf("'%s': '%s'", tmp->key, tmp->value);
+tmp = tmp->next;
+if (tmp != NULL)
+{
+band = 0;
+printf(", ");
+continue;
+}
+band = 1;
+}
+i++;
+}
+printf("}\n");
+}
+}
