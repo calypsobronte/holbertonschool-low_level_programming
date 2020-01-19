cfadab8302559bb10634504fadd4b49c0cfc8589
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2020-01-19 03:28:19 +0000

    add file task 2. key -> index gives you the index of a key.

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,1 +1,11 @@
 #include "hash_tables.h"
+/**
+ * key_index - that gives you the index of a key.
+ * @key: string of key
+ * @size: size of the array
+ * Return: value of a specific key
+*/
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	return (hash_djb2((unsigned char *)key) % size);
+}
