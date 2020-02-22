115928b47217327b62aa85107a58d356c64433f1
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2020-02-22 04:38:46 +0000

    [Fix - 5. Island Perimeter] error in conditional

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -10,11 +10,11 @@ def island_perimeter(grid):
                 if grid[x][y] == 1:
                     if (x - 1) == -1 or grid[x - 1][y] == 0:
                         perimeter += 1
-                    elif (x + 1) == len(grid) or grid[x + 1][y] == 0:
+                    if (x + 1) == len(grid) or grid[x + 1][y] == 0:
                         perimeter += 1
-                    elif (y - 1) == -1 or grid[x][y - 1] == 0:
+                    if (y - 1) == -1 or grid[x][y - 1] == 0:
                         perimeter += 1
-                    elif (y + 1) == len(grid[0]) or grid[x][y + 1] == 0:
+                    if (y + 1) == len(grid[0]) or grid[x][y + 1] == 0:
                         perimeter += 1
     return perimeter
 
