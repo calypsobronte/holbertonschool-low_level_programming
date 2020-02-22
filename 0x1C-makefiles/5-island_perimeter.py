8f9020a719b35ea4e88c6b0780e8e05a2cab38a8
Lina María Montaño Ramírez - Calypso Brontë 🦄 ◬ <codelinamaria@gmail.com>
2020-02-22 04:44:31 +0000

    [PEP8 - 5. Island Perimeter] blank line, least two spaces before inline comment, expected 2 blank lines

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python3
 # 5. Island Perimeter
 
+
 def island_perimeter(grid):
-    """ Returns the perimeter """
+    """ Returns the perimeter
+        x: Rows
+        y: Columns
+    """
+
     perimeter = 0
     if (len(grid) <= 100 and len(grid[0]) <= 100):
-        for x in range(len(grid)): # Rows
-            for y in range(len(grid[x])): # Columns
+        for x in range(len(grid)):
+            for y in range(len(grid[x])):
                 if grid[x][y] == 1:
                     if (x - 1) == -1 or grid[x - 1][y] == 0:
                         perimeter += 1
@@ -17,4 +22,3 @@ def island_perimeter(grid):
                     if (y + 1) == len(grid[0]) or grid[x][y + 1] == 0:
                         perimeter += 1
     return perimeter
-
